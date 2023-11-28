7d1f6d2518981928710771116465060a8e8d71ba
tveten <tveten@nr.no>
2023-11-28 16:33:26 +0100

    Add template + testing setup for pelt

---
+++ skchange/change_detectors/tests/test_pelt.py
@@ -0,0 +1,65 @@
+"""Simple PELT test."""
+
+import numpy as np
+import pandas as pd
+
+from sktime.annotation.clasp import ClaSPSegmentation
+from sktime.datasets import load_gun_point_segmentation
+from sktime.tests.test_switch import run_test_for_class
+from sktime.utils._testing.annotation import make_annotation_problem
+
+from skchange.change_detectors.pelt import Pelt
+
+
+def test_output_type():
+    """Test annotator output type."""
+    Estimator = Pelt
+    estimator = Estimator.create_test_instance()
+    if not run_test_for_class(Estimator):
+        return None
+
+    arg = make_annotation_problem(
+        n_timepoints=50, estimator_type=estimator.get_tag("distribution_type")
+    )
+    estimator.fit(arg)
+    arg = make_annotation_problem(
+        n_timepoints=10, estimator_type=estimator.get_tag("distribution_type")
+    )
+    y_pred = estimator.predict(arg)
+    assert isinstance(y_pred, (pd.Series, np.ndarray))
+
+
+def test_pelt_sparse():
+    """Test ClaSP sparse segmentation.
+
+    Check if the predicted change points match.
+    """
+    # load the test dataset
+    ts, period_size, cps = load_gun_point_segmentation()
+
+    # compute a ClaSP segmentation
+    clasp = ClaSPSegmentation(period_size, n_cps=1)
+    clasp.fit(ts)
+    found_cps = clasp.predict(ts)
+    scores = clasp.predict_scores(ts)
+
+    assert len(found_cps) == 1 and found_cps[0] == 893
+    assert len(scores) == 1 and scores[0] > 0.74
+
+
+def test_pelt_dense():
+    """Tests ClaSP dense segmentation.
+
+    Check if the predicted segmentation matches.
+    """
+    # load the test dataset
+    ts, period_size, cps = load_gun_point_segmentation()
+
+    # compute a ClaSP segmentation
+    clasp = ClaSPSegmentation(period_size, n_cps=1, fmt="dense")
+    clasp.fit(ts)
+    segmentation = clasp.predict(ts)
+    scores = clasp.predict_scores(ts)
+
+    assert len(segmentation) == 2 and segmentation[0].right == 893
+    assert np.argmax(scores) == 893
---
+++ skchange/tests/test_all_detectors.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+
+from sktime.utils.estimator_checks import check_estimator
+
+# from sktime.registry import all_estimators
+from sktime.tests.test_switch import run_test_for_class
+from sktime.utils.estimator_checks import check_estimator
+
+from skchange.change_detectors.pelt import Pelt
+
+# ALL_ANNOTATORS = all_estimators(estimator_types="series-annotator", return_names=False)
+ALL_ANNOTATORS = [Pelt]
+
+
+@pytest.mark.parametrize("Estimator", ALL_ANNOTATORS)
+def test_output_type(Estimator):
+    """Test annotator output type."""
+    if not run_test_for_class(Estimator):
+        return None
+
+    check_estimator(Estimator, raise_exceptions=True)
