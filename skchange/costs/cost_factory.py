17ce913fb5d95c1b21290eb11583b3a11e380ab5
tveten <tveten@nr.no>
2023-12-06 11:55:39 +0100

    Add documentation for costs

--- skchange/costs/cost_factory.py
+++ skchange/costs/cost_factory.py
@@ -1,12 +1,44 @@
+"""Factory for getting cost functions and their initializers from strings.
+
+Recipe for adding new costs:
+    1. Add a new module in skchange.costs: <cost_name>_cost.py
+    2. Add two functions:
+        init_<cost_name>_cost, which precomputes quantities that should be precomputed,
+        often partial sums and such.
+        <cost_name>_cost, which takes in the output of init_<cost_name>_cost as its
+        first argument, and starts and ends indices of the costs to be computed as its
+        second and third arguments.
+    3. Add the name of the cost to VALID_COSTS below.
+    4. Add the name of the cost to the docstring of cost_factory below.
+    4. Add a new if-statement in cost_factory below.
+
+"""
+
 from skchange.costs.l2_cost import init_l2_cost, l2_cost
 
 VALID_COSTS = ["l2"]
 
 
 def cost_factory(cost_name: str):
+    """Return cost function and its initializer.
+
+    Parameters
+    ----------
+    cost_name : str
+        Name of cost function. Must be one of 'l2'.
+
+    Returns
+    -------
+    cost_func : Callable
+        Cost function.
+    init_cost_func : Callable
+        Cost function initializer.
+    """
     if cost_name == "l2":
         return l2_cost, init_l2_cost
     else:
-        raise ValueError(
-            f"cost_name={cost_name} not recognized. Must be one of {', '.join(VALID_COSTS)}"
+        message = (
+            f"cost_name={cost_name} not recognized."
+            + f" Must be one of {', '.join(VALID_COSTS)}"
         )
+        raise ValueError(message)
--- skchange/costs/l2_cost.py
+++ skchange/costs/l2_cost.py
@@ -1,3 +1,5 @@
+"""L2 cost function for change point detection."""
+
 from typing import Tuple
 
 import numpy as np
@@ -9,6 +11,22 @@ from skchange.utils.numba.stats import col_cumsum
 
 @njit
 def init_l2_cost(X: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Precompute sums and weights for l2_cost.
+
+    Parameters
+    ----------
+    X : np.ndarray
+        2D array.
+
+    Returns
+    -------
+    sums : np.ndarray
+        Cumulative sums of X.
+    sums2 : np.ndarray
+        Cumulative sums of X**2.
+    weights : np.ndarray
+        Weights for sums2 in the cost calculation.
+    """
     n = X.shape[0]
     p = X.shape[1]
 
@@ -21,33 +39,28 @@ def init_l2_cost(X: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     return sums, sums2, weights
 
 
-# @njit
-# def l2_cost(
-#     precomputed_params: Tuple[np.ndarray, np.ndarray, np.ndarray],
-#     starts: list,
-#     ends: list,
-# ) -> np.ndarray:
-#     if len(ends) == 1:
-#         ends = np.repeat(ends, len(starts))
-
-#     sums, sums2, weights = precomputed_params
-
-#     costs = np.zeros(len(starts))
-#     for i in prange(len(starts)):
-#         start, end = starts[i], ends[i]
-#         partial_sum = sums[end + 1] - sums[start]
-#         partial_sum2 = sums2[end + 1] - sums2[start]
-#         weight = weights[end - start + 1]
-#         costs[i] = np.sum(partial_sum2 - partial_sum**2 / weight)
-#     return costs
-
-
 @njit
 def l2_cost(
     precomputed_params: Tuple[np.ndarray, np.ndarray, np.ndarray],
     starts: np.ndarray,
     ends: np.ndarray,
 ) -> np.ndarray:
+    """Calculate the l2 cost for each segment.
+
+    Parameters
+    ----------
+    precomputed_params : Tuple[np.ndarray, np.ndarray, np.ndarray]
+        Precomputed parameters from init_l2_cost.
+    starts : np.ndarray
+        Start indices of the segments.
+    ends : np.ndarray
+        End indices of the segments.
+
+    Returns
+    -------
+    costs : np.ndarray
+        Costs for each segment.
+    """
     sums, sums2, weights = precomputed_params
     partial_sums = sums[ends + 1] - sums[starts]
     partial_sums2 = sums2[ends + 1] - sums2[starts]
