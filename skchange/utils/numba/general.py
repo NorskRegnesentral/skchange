c1c5eb4373f9f1f3d6ee7dc7ae51dd8bdb3aa6d1
tveten <tveten@nr.no>
2023-12-06 09:12:41 +0100

    Document numba utilities

--- skchange/utils/numba/general.py
+++ skchange/utils/numba/general.py
@@ -1,9 +1,18 @@
+"""Numba-optimized functions for various common data manipulation tasks.""" ""
+
 import numpy as np
 from numba import njit, prange
 
 
 @njit
 def col_repeat(x: np.ndarray, n: int) -> np.ndarray:
+    """Repeat each column of a 2D array n times.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        2D array.
+    """
     expanded_x = np.zeros((x.shape[0], n))
     for j in prange(n):
         expanded_x[:, j] = x
@@ -12,6 +21,13 @@ def col_repeat(x: np.ndarray, n: int) -> np.ndarray:
 
 @njit
 def row_repeat(x: np.ndarray, n: int) -> np.ndarray:
+    """Repeat each row of a 2D array n times.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        2D array.
+    """
     expanded_x = np.zeros((x.shape[0], n))
     for i in prange(n):
         expanded_x[i, :] = x
