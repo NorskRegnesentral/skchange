bc75eca4b349ade850c887c982a09b76deaf67b7
tveten <tveten@nr.no>
2023-12-06 16:24:12 +0100

    Update recipe for adding scores

--- skchange/scores/score_factory.py
+++ skchange/scores/score_factory.py
@@ -1,16 +1,16 @@
 """Factory for getting test statistic functions and their initializers from strings.
 
-Recipe for adding new test statistics (replace "cost" with "test statistic" below):
-    1. Add a new module in skchange.costs: <cost_name>_cost.py
+Recipe for adding new scores (replace "score" with "score" below):
+    1. Add a new module in skchange.score: <score_name>_score.py
     2. Add two functions:
-        init_<cost_name>_cost, which precomputes quantities that should be precomputed,
-        often partial sums and such.
-        <cost_name>_cost, which takes in the output of init_<cost_name>_cost as its
-        first argument, and starts and ends indices of the costs to be computed as its
+        init_<score_name>_score, which precomputes quantities that should be
+        precomputed, often partial sums and such.
+        <score_name>_score, which takes in the output of init_<score_name>_score as its
+        first argument, and starts and ends indices of the scores to be computed as its
         second and third arguments.
-    3. Add the name of the cost to VALID_COSTS below.
-    4. Add the name of the cost to the docstring of cost_factory below.
-    4. Add a new if-statement in cost_factory below.
+    3. Add the name of the score to VALID_SCORES below.
+    4. Add the name of the score to the docstring of score_factory below.
+    4. Add a new if-statement in score_factory below.
 
 """
 
