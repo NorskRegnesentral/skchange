2c7ae3465e3d68eea2aa3d24b1c68b2a11334f8d
tveten <tveten@nr.no>
2023-12-07 14:14:59 +0100

    Clean up tests folder

--- skchange/tests/test_all_detectors.py
+++ skchange/tests/test_all_detectors.py
@@ -1,15 +1,9 @@
 import pytest
-
-# from sktime.registry import all_estimators
 from sktime.tests.test_switch import run_test_for_class
 from sktime.utils.estimator_checks import check_estimator
 
 from skchange.change_detectors.tests.test_change_detectors import change_detectors
 
-# todo:
-# ALL_ANNOTATORS = all_estimators(
-#     estimator_types="series-annotator", return_names=False
-# )
 all_annotators = change_detectors
 
 
