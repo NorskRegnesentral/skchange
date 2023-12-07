03ad156baa8bf8dd5eb2b5259022e0d0314b56a9
tveten <tveten@nr.no>
2023-12-07 09:26:38 +0100

    Get list of annotators from change detector tests

--- skchange/tests/test_all_detectors.py
+++ skchange/tests/test_all_detectors.py
@@ -4,16 +4,16 @@ import pytest
 from sktime.tests.test_switch import run_test_for_class
 from sktime.utils.estimator_checks import check_estimator
 
-from skchange.change_detectors.pelt import Pelt
+from skchange.change_detectors.tests.test_change_detectors import change_detectors
 
 # todo:
 # ALL_ANNOTATORS = all_estimators(
 #     estimator_types="series-annotator", return_names=False
 # )
-ALL_ANNOTATORS = [Pelt]
+all_annotators = change_detectors
 
 
-@pytest.mark.parametrize("Estimator", ALL_ANNOTATORS)
+@pytest.mark.parametrize("Estimator", all_annotators)
 def test_output_type(Estimator):
     """Test annotator output type."""
     if not run_test_for_class(Estimator):
