9c32a1ff409b4a76426059b74923a899efe9be42
tveten <tveten@nr.no>
2023-12-26 20:34:51 +0100

    Remove streamchange comparisons

--- interactive/explore_capa.py
+++ interactive/explore_capa.py
@@ -1,21 +1,12 @@
 import plotly.express as px
-from streamchange.capa import Capa as StreamchangeCapa
-from streamchange.capa import ConstMeanL2
 from streamchange.utils import Profiler
 
 from skchange.anomaly_detectors.capa import Capa
 from skchange.anomaly_detectors.mvcapa import Mvcapa
 from skchange.datasets.generate import teeth
 
-# Unviariate, compare to streamchange
-# streamchange
+# Unviariate
 df = teeth(n_segments=5, mean=10, segment_length=10, p=1, random_state=2)
-streamchange_capa = StreamchangeCapa(
-    ConstMeanL2(), minsl=2, maxsl=20, predict_point_anomalies=True
-)
-anomalies = streamchange_capa.fit_predict(df)
-
-# skchange
 capa = Capa(fmt="sparse", max_segment_length=20)
 anomalies = capa.fit_predict(df)
 
@@ -42,7 +33,6 @@ anomalies.plot(kind="line", backend="plotly")
 
 
 # Profiling
-# TODO: Add pruning?
 n = int(1e5)
 df = teeth(n_segments=1, mean=0, segment_length=n, p=1)
 detector = Capa(
@@ -54,7 +44,6 @@ detector = Mvcapa(
     collective_penalty_scale=5,
     point_penalty_scale=5,
 )
-detector = StreamchangeCapa(ConstMeanL2(), maxsl=1000, predict_point_anomalies=True)
 profiler = Profiler()
 profiler.start()
 detector.fit_predict(df)
