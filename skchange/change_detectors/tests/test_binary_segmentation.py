ce47ffa821b21be5a07bb789f9122ac18ec1d937
tveten <tveten@nr.no>
2023-12-29 22:02:28 +0100

    Add tests for parameter and data validators

---
+++ skchange/change_detectors/tests/test_binary_segmentation.py
@@ -0,0 +1,39 @@
+"""Tests for Moscore and all available scores."""
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from skchange.change_detectors.binary_segmentation import SeededBinarySegmentation
+
+
+def test_invalid_parameters():
+    """Test invalid input parameters to SeededBinarySegmentation.
+
+    These tests serve as tests for the input validators.
+    """
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(threshold_scale=-0.1)
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(min_interval_length=1)
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(min_interval_length=None)
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(min_interval_length=10, max_interval_length=9)
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(growth_factor=1.0)
+    with pytest.raises(ValueError):
+        SeededBinarySegmentation(growth_factor=2.01)
+
+
+def test_invalid_data():
+    """Test invalid input data to SeededBinarySegmentation.
+
+    These tests serve as tests for the input validators.
+    """
+    detector = SeededBinarySegmentation()
+    with pytest.raises(ValueError):
+        detector.fit(np.array([1.0]))
+
+    with pytest.raises(ValueError):
+        detector.fit(pd.Series([1.0, np.nan, 1.0, 1.0]))
