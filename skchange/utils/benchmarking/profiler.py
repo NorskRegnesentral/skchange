3c634ae98290ef66eb59d0cc1ef432e9ef394253
tveten <tveten@nr.no>
2024-01-12 11:11:00 +0100

    Add profiler to utils

---
+++ skchange/utils/benchmarking/profiler.py
@@ -0,0 +1,26 @@
+"""Utilities to profile code"""
+
+import cProfile
+import io
+import pstats
+from pstats import SortKey
+
+
+class Profiler:
+    """Profiler class to profile code"""
+
+    def __init__(self):
+        self.pr = cProfile.Profile()
+        pass
+
+    def start(self):
+        self.pr.enable()
+        return self
+
+    def stop(self):
+        self.pr.disable()
+        s = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(self.pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
