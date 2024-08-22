0fb4992336145400e5f95b68891f93d07b51ab25
tveten <tveten@nr.no>
2024-08-22 15:30:36 +0200

    Fix changepoint tests for new interface

--- skchange/change_detectors/tests/test_change_detectors.py
+++ skchange/change_detectors/tests/test_change_detectors.py
@@ -1,6 +1,5 @@
 """Basic tests for all change detectors."""
 
-import pandas as pd
 import pytest
 
 from skchange.change_detectors.moscore import Moscore
@@ -12,79 +11,57 @@ change_detectors = [Moscore, Pelt, SeededBinarySegmentation]
 
 
 @pytest.mark.parametrize("Estimator", change_detectors)
-def test_change_detector_sparse_int(Estimator):
-    """Test sparse int_label segmentation."""
+def test_change_detector_predict(Estimator):
+    """Test changepoint detector predict (sparse output)."""
     n_segments = 2
     seg_len = 50
     df = generate_teeth_data(
         n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=2
     )
     detector = Estimator.create_test_instance()
-    detector.set_params(fmt="sparse", labels="int_label")
     changepoints = detector.fit_predict(df)
     assert len(changepoints) == n_segments - 1 and changepoints[0] == seg_len - 1
 
 
 @pytest.mark.parametrize("Estimator", change_detectors)
-def test_change_detector_sparse_indicator(Estimator):
-    """Test sparse indicator segmentation."""
-    n_segments = 2
-    seg_len = 50
-    df = generate_teeth_data(
-        n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=3
-    )
-    detector = Estimator.create_test_instance()
-    detector.set_params(fmt="sparse", labels="indicator")
-    changepoints = detector.fit_predict(df)
-    assert len(changepoints) == n_segments - 1 and changepoints[0] == seg_len - 1
-
-
-@pytest.mark.parametrize("Estimator", change_detectors)
-def test_change_detector_score(Estimator):
-    """Test sparse score segmentation."""
-    n_segments = 2
-    seg_len = 50
-    df = generate_teeth_data(
-        n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=4
-    )
-    sparse_detector = Estimator.create_test_instance()
-    sparse_detector.set_params(fmt="sparse", labels="score")
-    dense_detector = Estimator.create_test_instance()
-    dense_detector.set_params(fmt="dense", labels="score")
-    sparse_scores = sparse_detector.fit_predict(df)
-    dense_scores = dense_detector.fit_predict(df)
-    assert (sparse_scores == dense_scores).all(axis=None)
-    if isinstance(sparse_scores, pd.DataFrame):
-        assert "score" in sparse_scores.columns
-    else:
-        assert sparse_scores.name == "score"
-
-
-@pytest.mark.parametrize("Estimator", change_detectors)
-def test_change_detector_dense_int(Estimator):
-    """Tests dense int_label segmentation."""
+def test_change_detector_transform(Estimator):
+    """Test changepoint detector transform (dense output)."""
     n_segments = 2
     seg_len = 50
     df = generate_teeth_data(
         n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=2
     )
     detector = Estimator.create_test_instance()
-    detector.set_params(fmt="dense", labels="int_label")
-    labels = detector.fit_predict(df)
+    labels = detector.fit_transform(df)
     assert labels.nunique() == n_segments
     assert labels[seg_len - 1] == 0.0 and labels[seg_len] == 1.0
 
 
 @pytest.mark.parametrize("Estimator", change_detectors)
-def test_change_detector_dense_indicator(Estimator):
-    """Tests dense indicator segmentation."""
+def test_change_detector_sparse_to_dense(Estimator):
+    """Test that predict + sparse_to_dense == transform."""
     n_segments = 2
     seg_len = 50
     df = generate_teeth_data(
-        n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=8
+        n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=2
     )
     detector = Estimator.create_test_instance()
-    detector.set_params(fmt="dense", labels="indicator")
-    cpt_indicator = detector.fit_predict(df)
-    assert cpt_indicator.sum() == n_segments - 1
-    assert cpt_indicator[seg_len - 1]
+    changepoints = detector.fit_predict(df)
+    labels = detector.sparse_to_dense(changepoints, df.index)
+    labels_transform = detector.fit_transform(df)
+    assert labels.equals(labels_transform)
+
+
+@pytest.mark.parametrize("Estimator", change_detectors)
+def test_change_detector_dense_to_sparse(Estimator):
+    """Test that transform + dense_to_sparse == predict."""
+    n_segments = 2
+    seg_len = 50
+    df = generate_teeth_data(
+        n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=2
+    )
+    detector = Estimator.create_test_instance()
+    labels = detector.fit_transform(df)
+    changepoints = detector.dense_to_sparse(labels)
+    changepoints_predict = detector.fit_predict(df)
+    assert changepoints.equals(changepoints_predict)
