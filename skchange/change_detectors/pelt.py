5da1cbdf439d9aaa7cfac9b435c60cc5371cf505
tveten <tveten@nr.no>
2024-08-22 10:29:23 +0200

    Conform Pelt to BaseDetector

--- skchange/change_detectors/pelt.py
+++ skchange/change_detectors/pelt.py
@@ -9,9 +9,8 @@ from typing import Callable, Optional, Union
 import numpy as np
 import pandas as pd
 from numba import njit
-from sktime.annotation.base import BaseSeriesAnnotator
 
-from skchange.change_detectors.utils import format_changepoint_output
+from skchange.change_detectors.base import ChangepointDetector
 from skchange.costs.cost_factory import cost_factory
 from skchange.utils.validation.data import check_data
 from skchange.utils.validation.parameters import check_larger_than
@@ -62,7 +61,7 @@ def run_pelt(
     return opt_cost[1:], get_changepoints(prev_cpts)
 
 
-class Pelt(BaseSeriesAnnotator):
+class Pelt(ChangepointDetector):
     """Pruned exact linear time changepoint detection.
 
     An efficient implementation of the PELT algorithm [1]_ for changepoint detection.
@@ -80,18 +79,6 @@ class Pelt(BaseSeriesAnnotator):
         input to .fit() (not supported yet).
     min_segment_length : int, optional (default=2)
         Minimum length of a segment.
-    fmt : str {"dense", "sparse"}, optional (default="sparse")
-        Annotation output format:
-        * If "sparse", a sub-series of labels for only the outliers in X is returned,
-        * If "dense", a series of labels for all values in X is returned.
-    labels : str {"indicator", "score", "int_label"}, optional (default="int_label")
-        Annotation output labels:
-        * If "indicator", returned values are boolean, indicating whether a value is an
-        outlier,
-        * If "score", returned values are floats, giving the outlier score.
-        * If "int_label", returned values are integer, indicating which segment a value
-        belongs to.
-
 
     References
     ----------
@@ -120,13 +107,11 @@ class Pelt(BaseSeriesAnnotator):
         cost: Union[str, Callable] = "mean",
         penalty_scale: Optional[float] = 2.0,
         min_segment_length: int = 2,
-        fmt: str = "sparse",
-        labels: str = "int_label",
     ):
         self.cost = cost
         self.penalty_scale = penalty_scale
         self.min_segment_length = min_segment_length
-        super().__init__(fmt=fmt, labels=labels)
+        super().__init__()
         self.cost_func, self.cost_init_func = cost_factory(self.cost)
 
         check_larger_than(0, penalty_scale, "penalty_scale", allow_none=True)
@@ -223,17 +208,35 @@ class Pelt(BaseSeriesAnnotator):
             min_length=2 * self.min_segment_length,
             min_length_name="2*min_segment_length",
         )
-        opt_costs, self.changepoints = run_pelt(
+        opt_costs, changepoints = run_pelt(
             X.values,
             self.cost_func,
             self.cost_init_func,
             self.penalty_,
             self.min_segment_length,
         )
+        # Store the scores for introspection without recomputing using score_transform
         self.scores = pd.Series(opt_costs, index=X.index, name="score")
-        return format_changepoint_output(
-            self.fmt, self.labels, self.changepoints, X.index, self.scores
-        )
+        return ChangepointDetector._format_sparse_output(changepoints)
+
+    def _score_transform(self, X: Union[pd.DataFrame, pd.Series]) -> pd.Series:
+        """Compute the pelt scores for the input data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame - data to compute scores for, time series
+
+        Returns
+        -------
+        scores : pd.Series - scores for sequence X
+
+        Notes
+        -----
+        The PELT scores are the cumulative optimal costs, so the scores are increasing
+        and are not per observation scores.
+        """
+        self.predict(X)
+        return self.scores
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
