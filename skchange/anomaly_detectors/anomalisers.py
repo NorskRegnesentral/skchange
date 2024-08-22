1e3f5401de612257e041fa9b12c6fcf40156d689
tveten <tveten@nr.no>
2024-08-22 21:23:35 +0200

    Conform Anomaliser to BaseDetector

--- skchange/anomaly_detectors/anomalisers.py
+++ skchange/anomaly_detectors/anomalisers.py
@@ -4,17 +4,17 @@ from typing import Callable, Optional, Union
 
 import numpy as np
 import pandas as pd
-from sktime.annotation.base import BaseSeriesAnnotator
 
-from skchange.anomaly_detectors.utils import format_anomaly_output
+from skchange.anomaly_detectors.base import CollectiveAnomalyDetector
+from skchange.change_detectors.base import ChangepointDetector
 
 
-class StatThresholdAnomaliser(BaseSeriesAnnotator):
+class StatThresholdAnomaliser(CollectiveAnomalyDetector):
     """Anomaly detection based on thresholding the values of segment statistics.
 
     Parameters
     ----------
-    change_detector : BaseSeriesAnnotator
+    change_detector : ChangepointDetector
         Change detector to use for detecting segments.
     stat : callable, optional (default=np.mean)
         Statistic to calculate per segment.
@@ -32,32 +32,30 @@ class StatThresholdAnomaliser(BaseSeriesAnnotator):
 
     def __init__(
         self,
-        change_detector: BaseSeriesAnnotator,
+        change_detector: ChangepointDetector,
         stat: Callable = np.mean,
         stat_lower: float = -1.0,
         stat_upper: float = 1.0,
-        fmt: str = "sparse",
-        labels: str = "indicator",
     ):
         self.change_detector = change_detector
         self.stat = stat
         self.stat_lower = stat_lower
         self.stat_upper = stat_upper
-        super().__init__(fmt=fmt, labels=labels)
+        super().__init__()
 
         if self.stat_lower > self.stat_upper:
             message = f"stat_lower ({self.stat_lower}) must be less"
             +f" than or equal to stat_upper ({self.stat_upper})."
             raise ValueError(message)
 
-    def _fit(self, X: pd.DataFrame, Y: Optional[pd.DataFrame] = None):
+    def _fit(self, X: pd.DataFrame, y: Optional[pd.DataFrame] = None):
         """Fits the change detector to training data.
 
         Parameters
         ----------
         X : pd.DataFrame
             training data to fit the threshold to.
-        Y : pd.Series, optional
+        y : pd.Series, optional
             Does nothing. Only here to make the fit method compatible with sktime
             and scikit-learn.
 
@@ -65,7 +63,8 @@ class StatThresholdAnomaliser(BaseSeriesAnnotator):
         -------
         self : returns a reference to self
         """
-        self.change_detector.fit(X, Y)
+        self.change_detector_ = self.change_detector.clone()
+        self.change_detector_.fit(X, y)
         return self
 
     def _predict(self, X: Union[pd.DataFrame, pd.Series]) -> pd.Series:
@@ -77,28 +76,19 @@ class StatThresholdAnomaliser(BaseSeriesAnnotator):
 
         Returns
         -------
-        Y : pd.Series - annotations for sequence X
+        y : pd.Series - annotations for sequence X
             exact format depends on annotation type
         """
         # This is the required output format for the rest of the code to work.
-        self.change_detector.fmt = "dense"
-        self.change_detector.labels = "int_label"
-        self.segments = self.change_detector.predict(X)
-
-        df = pd.concat([X, self.segments], axis=1)
-        self.anomalies = []
-        for _, segment in df.reset_index(drop=True).groupby("int_label"):
+        segments = self.change_detector_.transform(X)
+        df = pd.concat([X, segments], axis=1)
+        anomalies = []
+        for _, segment in df.reset_index(drop=True).groupby("segment_label"):
             segment_stat = self.stat(segment.iloc[:, 0].values)
             if (segment_stat < self.stat_lower) | (segment_stat > self.stat_upper):
-                self.anomalies.append((int(segment.index[0]), int(segment.index[-1])))
+                anomalies.append((int(segment.index[0]), int(segment.index[-1])))
 
-        return format_anomaly_output(
-            self.fmt,
-            self.labels,
-            X.index,
-            self.anomalies,
-            scores=self.change_detector.scores,
-        )
+        return CollectiveAnomalyDetector._format_sparse_output(anomalies)
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
@@ -123,10 +113,16 @@ class StatThresholdAnomaliser(BaseSeriesAnnotator):
 
         params = [
             {
-                "change_detector": Moscore(bandwidth=10),
+                "change_detector": Moscore(bandwidth=3),
                 "stat": np.mean,
                 "stat_lower": -1.0,
                 "stat_upper": 1.0,
+            },
+            {
+                "change_detector": Moscore(bandwidth=5),
+                "stat": np.median,
+                "stat_lower": -2.0,
+                "stat_upper": 2.0,
             },
         ]
         return params
