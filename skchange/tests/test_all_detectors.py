00d23c5cc793c2329587247bb3b717ef35de96ad
tveten <tveten@nr.no>
2024-08-22 09:38:54 +0200

    Use parametrize_with_checks from sktime

--- skchange/tests/test_all_detectors.py
+++ skchange/tests/test_all_detectors.py
@@ -1,19 +1,13 @@
 """Tests for all annotators/detectors in skchange."""
 
-import pytest
-from sktime.tests.test_switch import run_test_for_class
-from sktime.utils.estimator_checks import check_estimator
+from sktime.utils.estimator_checks import check_estimator, parametrize_with_checks
 
 from skchange.anomaly_detectors.tests.test_anomaly_detectors import anomaly_detectors
 from skchange.change_detectors.tests.test_change_detectors import change_detectors
 
-all_annotators = anomaly_detectors + change_detectors
+all_detectors = anomaly_detectors + change_detectors
 
 
-@pytest.mark.parametrize("Estimator", all_annotators)
-def test_sktime_annotator_compatibility(Estimator):
-    """Check compatibility with sktime annotator interface."""
-    if not run_test_for_class(Estimator):
-        return None
-
-    check_estimator(Estimator, raise_exceptions=True)
+@parametrize_with_checks(all_detectors)
+def test_sktime_compatible_estimators(obj, test_name):
+    check_estimator(obj, tests_to_run=test_name, raise_exceptions=True)
