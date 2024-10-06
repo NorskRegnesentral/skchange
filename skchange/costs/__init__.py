0416ad79147074b061c9920b79dec75ed85bcdf3
tveten <tveten@nr.no>
2024-10-06 21:44:41 +0200

    feat(api): add costs and savings functions to init

--- skchange/costs/__init__.py
+++ skchange/costs/__init__.py
@@ -1,1 +1,23 @@
 """Cost functions for cost-based change and anomaly detection."""
+
+from skchange.costs.cost_factory import (
+    VALID_COSTS,
+    cost_factory,
+)
+from skchange.costs.mean_cost import init_mean_cost, mean_cost
+from skchange.costs.mean_saving import init_mean_saving, mean_saving
+from skchange.costs.saving_factory import (
+    VALID_SAVINGS,
+    saving_factory,
+)
+
+__all__ = [
+    init_mean_cost,
+    mean_cost,
+    init_mean_saving,
+    mean_saving,
+    cost_factory,
+    saving_factory,
+    VALID_COSTS,
+    VALID_SAVINGS,
+]
