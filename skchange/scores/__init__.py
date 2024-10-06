5c483bd4ded9a78636606fa7ca56652e0a202056
tveten <tveten@nr.no>
2024-10-06 21:38:54 +0200

    feat(api): add score functions to init

--- skchange/scores/__init__.py
+++ skchange/scores/__init__.py
@@ -1,1 +1,31 @@
 """Test statistic functions for hypotesis testing-based change and anomaly detection."""
+
+from skchange.scores.mean_cov_score import (
+    init_mean_cov_score,
+    mean_cov_score,
+)
+from skchange.scores.mean_score import init_mean_score, mean_anomaly_score, mean_score
+from skchange.scores.mean_var_score import (
+    init_mean_var_score,
+    mean_var_anomaly_score,
+    mean_var_score,
+)
+from skchange.scores.score_factory import (
+    VALID_ANOMALY_SCORES,
+    VALID_CHANGE_SCORES,
+    score_factory,
+)
+
+__all__ = [
+    init_mean_score,
+    mean_score,
+    init_mean_var_score,
+    mean_var_score,
+    init_mean_cov_score,
+    mean_cov_score,
+    score_factory,
+    mean_anomaly_score,
+    mean_var_anomaly_score,
+    VALID_CHANGE_SCORES,
+    VALID_ANOMALY_SCORES,
+]
