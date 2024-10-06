7e6b12fbcfc5e6c3a366d83b83dd17c98d545962
tveten <tveten@nr.no>
2024-10-06 21:46:41 +0200

    feat(api): add dataset functions to init

--- skchange/datasets/__init__.py
+++ skchange/datasets/__init__.py
@@ -1,1 +1,13 @@
 """Datasets and dataset generators for skchange."""
+
+from skchange.datasets.generate import (
+    add_linspace_outliers,
+    generate_anomalous_data,
+    generate_teeth_data,
+)
+
+__all__ = [
+    "add_linspace_outliers",
+    "generate_anomalous_data",
+    "generate_teeth_data",
+]
