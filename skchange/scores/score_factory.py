47ca97c9b003e9af60346f83ee5a47e2db860146
tveten <tveten@nr.no>
2024-10-14 10:58:36 +0200

    fix: Add anomaly_score_factory which was deleted by mistake

--- skchange/scores/score_factory.py
+++ skchange/scores/score_factory.py
@@ -24,9 +24,10 @@ from skchange.scores.mean_cov_score import (
     init_mean_cov_score,
     mean_cov_score,
 )
-from skchange.scores.mean_score import init_mean_score, mean_score
+from skchange.scores.mean_score import init_mean_score, mean_anomaly_score, mean_score
 from skchange.scores.mean_var_score import (
     init_mean_var_score,
+    mean_var_anomaly_score,
     mean_var_score,
 )
 
@@ -34,7 +35,7 @@ VALID_CHANGE_SCORES = ["mean", "mean_var", "mean_cov"]
 VALID_ANOMALY_SCORES = ["mean", "mean_var"]
 
 
-def score_factory(score: Union[str, tuple[Callable, Callable]]):
+def score_factory(score: Union[str, tuple[Callable, Callable]] = "mean"):
     """Return score function and its initializer.
 
     Parameters
@@ -93,6 +94,66 @@ def score_factory(score: Union[str, tuple[Callable, Callable]]):
         message = (
             f"score={score} not recognized."
             + f" Must be one of {', '.join(VALID_CHANGE_SCORES)}"
+            + " or a tuple of two numba jitted functions."
+        )
+        raise ValueError(message)
+
+
+def anomaly_score_factory(score: Union[str, tuple[Callable, Callable]] = "mean"):
+    """Return anomaly score function and its initializer.
+
+    Parameters
+    ----------
+    score: {"mean", "mean_var"}, tuple[Callable, Callable], default="mean"
+        Test statistic to use for anomaly detection.
+
+        * `"mean"`: The CUSUM statistic for a change in mean (this is equivalent to a
+          likelihood ratio test for a change in the mean of Gaussian data). For
+          multivariate data, the sum of the CUSUM statistics for each dimension is used.
+        * `"mean_var"`: The likelihood ratio test for a change in the mean and/or
+          variance of Gaussian data. For multivariate data, the sum of the likelihood
+          ratio statistics for each dimension is used.
+        * If a tuple, it must contain two numba jitted functions:
+
+            1. The first function is the scoring function, which takes five arguments:
+
+                1. `precomputed_params`: The output of the second function.
+                2. `interval_starts`: Start indices of the intervals to to test for
+                anomalies within.
+                3. `interval_ends`: End indices of the intervals to to test for
+                anomalies within.
+                4. `anomaly_starts`: Start indices of the anomalies.
+                5. `anomaly_ends`: End indices of the anomalies.
+
+            For each interval_start, interval_end, anomaly_start and anomaly_end, the
+            score should compare the data in `[anomaly_start:anomaly_end+1]` to the
+            data in `[interval_start:interval_end+1]` that is not in
+            `[anomaly_start:anomaly_end+1]`.
+
+            The overall and anomalous intervals must
+            satisfy `interval_start > anomaly_start <= anomaly_end <= interval_end`.
+
+            2. The second function is the initializer, which takes the data matrix as
+               input and returns precomputed quantities that may speed up the score
+               calculations. If not relevant, just return the data matrix.
+
+    Returns
+    -------
+    score_func : Numba jitted Callable
+        Score function.
+    init_score_func : Numba jitted Callable
+        Score function initializer.
+    """
+    if isinstance(score, str) and score == "mean":
+        return mean_anomaly_score, init_mean_score
+    elif isinstance(score, str) and score == "mean_var":
+        return mean_var_anomaly_score, init_mean_var_score
+    elif len(score) == 2 and all([is_jitted(s) for s in score]):
+        return score[0], score[1]
+    else:
+        message = (
+            f"score={score} not recognized."
+            + f" Must be one of {', '.join(VALID_ANOMALY_SCORES)}"
             + " or a tuple of two numba jitted functions."
         )
         raise ValueError(message)
