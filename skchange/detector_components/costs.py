446b864edac1b430ed2c16749974354d543a5059
tveten <tveten@nr.no>
2024-10-21 09:32:16 +0200

    refactor: Rename MeanCost to L2Cost

--- skchange/detector_components/costs.py
+++ skchange/detector_components/costs.py
@@ -9,19 +9,19 @@ from skchange.detector_components.base import BaseCost
 from skchange.detector_components.utils import init_sample_sizes, init_sums, init_sums2
 
 
-class MeanCost(BaseCost):
-    """Log-likelihood cost for a Gaussian mean with unit variance."""
+class L2Cost(BaseCost):
+    """L2 cost function."""
 
     def _build_jitted_precompute(self):
         @njit(cache=True)
-        def init_mean_cost(X: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        def init_l2_cost(X: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
             return init_sums(X), init_sums2(X), init_sample_sizes(X)
 
-        self.jitted_precompute = init_mean_cost
+        self.jitted_precompute = init_l2_cost
 
     def _build_jitted_compute(self):
         @njit(cache=True)
-        def mean_cost(
+        def l2_cost(
             precomputed: tuple[np.ndarray, np.ndarray, np.ndarray],
             starts: np.ndarray,
             ends: np.ndarray,
@@ -33,4 +33,4 @@ class MeanCost(BaseCost):
             costs = np.sum(partial_sums2 - partial_sums**2 / weights, axis=1)
             return costs
 
-        self.jitted_compute = mean_cost
+        self.jitted_compute = l2_cost
