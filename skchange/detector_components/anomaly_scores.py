7ff49d25c4ecedcd97c58c5015a9b75a5baadf6f
tveten <tveten@nr.no>
2024-10-21 09:49:25 +0200

    feat: add first version of cost based anomaly score

--- skchange/detector_components/anomaly_scores.py
+++ skchange/detector_components/anomaly_scores.py
@@ -1,1 +1,40 @@
 """Scores for anomaly detection."""
+
+import numpy as np
+from numba import njit
+
+from skchange.detector_components.base import BaseAnomalyScore, BaseCost
+
+
+class CostBasedAnomalyScore(BaseAnomalyScore):
+    """Anomaly score based on a cost function."""
+
+    def __init__(self, cost: BaseCost):
+        self.cost = cost
+        super().__init__()
+
+    def _build_jitted_precompute(self):
+        self.jitted_precompute = self.cost.jitted_precompute
+
+    def _build_jitted_compute(self):
+        cost_jitted_compute = self.cost.jitted_compute
+
+        @njit(cache=True)
+        def cost_based_anomaly_score(
+            precomputed: tuple[np.ndarray, np.ndarray, np.ndarray],
+            starts: np.ndarray,
+            ends: np.ndarray,
+            anomaly_starts: np.ndarray,
+            anomaly_ends: np.ndarray,
+        ) -> np.ndarray:
+            pre_anomaly_cost = cost_jitted_compute(
+                precomputed, starts, anomaly_starts - 1
+            )
+            anomaly_cost = cost_jitted_compute(
+                precomputed, anomaly_starts, anomaly_ends
+            )
+            post_anomaly_cost = cost_jitted_compute(precomputed, anomaly_ends + 1, ends)
+            full_cost = cost_jitted_compute(precomputed, starts, ends)
+            return full_cost - (pre_anomaly_cost + anomaly_cost + post_anomaly_cost)
+
+        self.jitted_compute = cost_based_anomaly_score
