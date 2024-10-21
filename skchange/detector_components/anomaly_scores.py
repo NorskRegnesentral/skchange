07621e79addae24783d20b97146257c8a01a3d5d
tveten <tveten@nr.no>
2024-10-21 23:04:29 +0200

    feat: make cost based anomaly score work with new base cost

--- skchange/detector_components/anomaly_scores.py
+++ skchange/detector_components/anomaly_scores.py
@@ -4,6 +4,7 @@ import numpy as np
 from numba import njit
 
 from skchange.detector_components.base import BaseAnomalyScore, BaseCost
+from skchange.detector_components.utils import identity_func
 
 
 class CostBasedAnomalyScore(BaseAnomalyScore):
@@ -14,23 +15,31 @@ class CostBasedAnomalyScore(BaseAnomalyScore):
         super().__init__()
 
     def _build_jitted_precompute(self):
-        self.jitted_precompute = self.cost.jitted_precompute
+        self.jitted_precompute = identity_func
 
     def _build_jitted_compute(self):
-        cost = self.cost.jitted_compute
+        generic_cost = self.cost.jitted_compute_generic
 
         @njit(cache=True)
         def cost_based_anomaly_score(
-            precomputed: tuple,
+            X: np.ndarray,
             starts: np.ndarray,
             ends: np.ndarray,
             anomaly_starts: np.ndarray,
             anomaly_ends: np.ndarray,
         ) -> np.ndarray:
-            pre_anomaly_cost = cost(precomputed, starts, anomaly_starts - 1)
-            anomaly_cost = cost(precomputed, anomaly_starts, anomaly_ends)
-            post_anomaly_cost = cost(precomputed, anomaly_ends + 1, ends)
-            full_cost = cost(precomputed, starts, ends)
-            return full_cost - (pre_anomaly_cost + anomaly_cost + post_anomaly_cost)
+            scores = np.zeros(len(starts), dtype=np.float64)
+            for i, start, end, anomaly_start, anomaly_end in zip(
+                range(len(starts)), starts, ends, anomaly_starts, anomaly_ends
+            ):
+                anomaly_cost = generic_cost(X[anomaly_start : anomaly_end + 1])
+                baseline_data = np.concatenate(
+                    (X[start:anomaly_start], X[anomaly_end + 1 : end + 1])
+                )
+                baseline_cost = generic_cost(baseline_data)
+                full_cost = generic_cost(X[start : end + 1])
+                scores[i] = full_cost - (baseline_cost + anomaly_cost)
+
+            return scores
 
         self.jitted_compute = cost_based_anomaly_score
