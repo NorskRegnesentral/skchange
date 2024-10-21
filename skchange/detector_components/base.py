8e97e692e63cc56c937bb1d1a8bfabbe0691515a
tveten <tveten@nr.no>
2024-10-21 23:02:05 +0200

    Add new base class for costs with more functionality

--- skchange/detector_components/base.py
+++ skchange/detector_components/base.py
@@ -5,9 +5,11 @@ __author__ = ["Tveten"]
 import inspect
 
 import numpy as np
+from numba import njit
 from numpy.typing import ArrayLike
 from skbase.base import BaseObject
 
+from skchange.detector_components.utils import identity_func
 from skchange.utils.validation.parameters import check_jitted
 
 
@@ -22,24 +24,16 @@ class BaseDetectorComponent(BaseObject):
         self._check_build()
 
     def _build_jitted_precompute(self):
+        # self.jitted_precompute = jitted_func
         raise NotImplementedError("abstract method")
-
-    def _build_jitted_compute(self):
-        raise NotImplementedError("abstract method")
-
-    def _check_build(self):
-        check_jitted(self.jitted_precompute)
-        check_jitted(self.jitted_compute)
-        self._check_jitted_precompute_input()
-        self._check_jitted_compute_input()
-        self._check_jitted_compute_output()
-        # No checks are performed on the precompute output and inputs.
-        # It is complicated to check this without being too restrictive.
-        # It is left to the numba compiler to handle for now.
 
     def precompute(self, X: ArrayLike) -> np.ndarray | tuple:
         """Precompute parameters for the detector component."""
         return self.jitted_precompute(np.asarray(X))
+
+    def _build_jitted_compute(self):
+        # self.jitted_compute = jitted_func
+        raise NotImplementedError("abstract method")
 
     def compute(self, precomputed: np.ndarray | tuple, *args) -> np.ndarray:
         """Compute the detector component.
@@ -52,6 +46,16 @@ class BaseDetectorComponent(BaseObject):
 
         """
         raise NotImplementedError("abstract method")
+
+    def _check_build(self):
+        check_jitted(self.jitted_precompute)
+        check_jitted(self.jitted_compute)
+        self._check_jitted_precompute_input()
+        self._check_jitted_compute_input()
+        self._check_jitted_compute_output()
+        # No checks are performed on the precompute output and inputs.
+        # It is complicated to check this without being too restrictive.
+        # It is left to the numba compiler to handle for now.
 
     def _check_jitted_precompute_input(self):
         """Check the input signature of jitted_precompute."""
@@ -93,6 +97,90 @@ class BaseDetectorComponent(BaseObject):
 
 
 class BaseCost(BaseDetectorComponent):
+    """Base class template for costs.
+
+    What should the naming convention be for the different functions?
+    Current name: Explanation/options.
+
+    - `compute`: Computes the optimal cost for several segments.
+    - `precompute`: Precomputes quantities used in `compute`.
+    - `compute_generic`: Computes the optimal cost for a data matrix.
+    - `compute_fixed`: Computes the cost with a fixed parameter for a data matrix.
+
+    """
+
+    def __init__(self):
+        self.jitted_compute_generic = None
+        self.jitted_compute_fixed = None
+        self._build_jitted_compute_generic()
+        self._build_jitted_compute_fixed()
+        # TODO: Add check for generic and fixed compute functions.
+        super().__init__()
+
+    def _build_jitted_compute_generic(self):
+        # @njit(cache=True)
+        # def generic_cost(x: np.ndarray) -> float:
+        # return cost
+        raise NotImplementedError("abstract method")
+
+    def compute_generic(self, X: ArrayLike) -> float:
+        """Directly Compute the cost for a data matrix."""
+        return self.jitted_compute_generic(np.asarray(X))
+
+    def _build_jitted_compute_fixed(self):
+        # param = self.param
+        #
+        # @njit(cache=True)
+        # def fixed_cost(x: np.ndarray, param=param) -> float:
+        # return cost
+        raise NotImplementedError("abstract method")
+
+    def compute_fixed(self, X: ArrayLike) -> float:
+        """Compute the cost with a fixed parameter for a data matrix."""
+        return self.jitted_compute_fixed(np.asarray(X))
+
+    def _build_jitted_precompute(self):
+        self.jitted_precompute = identity_func
+
+    def _build_jitted_compute(self):
+        generic_cost = self.jitted_compute_generic
+
+        @njit(cache=True)
+        def default_compute(
+            X: np.ndarray,
+            starts: np.ndarray,
+            ends: np.ndarray,
+        ) -> np.ndarray:
+            costs = np.zeros(len(starts), dtype=np.float64)
+            for i, start, end in zip(range(len(starts)), starts, ends):
+                costs[i] = generic_cost(X[start : end + 1])
+            return costs
+
+        self.jitted_compute = default_compute
+
+    def compute(
+        self, precomputed: np.ndarray | tuple, starts: np.ndarray, ends: np.ndarray
+    ) -> np.ndarray:
+        """Compute the cost for each segment.
+
+        Parameters
+        ----------
+        precomputed : `tuple`
+            Precomputed parameters from `jitted_precompute`.
+        starts : `np.ndarray`
+            Start indices of the segments (inclusive).
+        ends : `np.ndarray`
+            End indices of the segments (inclusive).
+
+        Returns
+        -------
+        costs : `np.ndarray`
+            Costs for each segment.
+        """
+        return self.jitted_compute(precomputed, starts, ends)
+
+
+class BaseCostOld(BaseDetectorComponent):
     """Base class template for costs."""
 
     def compute(
@@ -151,7 +239,8 @@ class BaseAnomalyScore(BaseDetectorComponent):
 class BaseSaving(BaseDetectorComponent):
     """Base class template for savings.
 
-    Savings are anomaly scores where the baseline parameters are assumed known.
+    Savings are anomaly scores where the baseline parameters are assumed known and
+    global.
     """
 
     def compute(
--- skchange/detector_components/costs.py
+++ skchange/detector_components/costs.py
@@ -1,15 +1,117 @@
-"""Cost functions."""
+"""Cost functions.
+
+What functions would be useful in cost functions.
+
+- Compute cost over a segment (start, end).
+- Compute joint cost over several disjoint segments. Useful for anomaly_score for
+  example, where the cost need to be calculated for disjoint segments.
+- Compute cost for any input data vector. Will work for any, also unforeseen,
+  applications.
+  No need/not possible to do precomputations of the type used for segments.
+- Compute the cost for both fixed and estimated parameters. For example in L2, both
+  for a fixed mean and for the maximum likelihood mean. Should this be two separate
+  cost classes, or combined in one by using a parameter in the class constructor?
+
+
+Want in addition:
+
+- Option to write specialised detector components for common costs/scores,
+  like mean, variance, etc. E.g. through precomputing quantities or through simplifying
+  score expressions that involve several independent calls to the cost.
+- Upon implementation of a new cost, it can be used to generate all the other types of
+  components: change_scores, anomaly_scores, savings.
+- Savings: Want to specify the cost and the baseline parameter outside the detector.
+    Options:
+
+    * Specify two separate cost functions. Need to check that one cost is a fixed
+    version of the other somehow. Maybe this is the cleanest solution?
+    * Specify the cost function, and the cost function contains both a fixed and
+    maximum likelihood/estimate version.
+
+        - Specify the baseline parameter upon construction, through init. A bit clunky
+        to set the parameter for one of the functions.
+        - Expose a parameter argument in the jitted fixed function. Will get messy
+        for different costs.
+"""
 
 __author__ = ["Tveten"]
 
 import numpy as np
 from numba import njit
 
-from skchange.detector_components.base import BaseCost
+from skchange.detector_components.base import BaseCost, BaseCostOld
 from skchange.detector_components.utils import init_sample_sizes, init_sums, init_sums2
 
 
 class L2Cost(BaseCost):
+    """L2 cost function.
+
+    Parameters
+    ----------
+    fixed_mean : `float` or `np.ndarray`, default=0.0
+        The default value of `mean` in `compute_fixed`. If a float, the same mean is
+        used for all dimensions. If an array, the mean should have the same number of
+        dimensions as the input data.
+    """
+
+    def __init__(self, fixed_mean: float | np.ndarray = 0.0):
+        self.fixed_mean = fixed_mean
+        self._fixed_mean = np.asarray(self.fixed_mean).reshape(-1)
+
+    def _build_jitted_precompute(self):
+        # Could be a default implementation in BaseCost that returns the input.
+        @njit(cache=True)
+        def init_l2_cost(X: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+            return init_sums(X), init_sums2(X), init_sample_sizes(X)
+
+        self.jitted_precompute = init_l2_cost
+
+    def _build_jitted_compute(self):
+        # Very fast when applicable.
+        # Could be a default implementation in BaseCost that uses the generic compute.
+        @njit(cache=True)
+        def l2_cost(
+            precomputed: tuple[np.ndarray, np.ndarray, np.ndarray],
+            starts: np.ndarray,
+            ends: np.ndarray,
+        ) -> np.ndarray:
+            sums, sums2, weights = precomputed
+            partial_sums = sums[ends + 1] - sums[starts]
+            partial_sums2 = sums2[ends + 1] - sums2[starts]
+            weights = weights[ends - starts + 1]
+            cost_matrix = partial_sums2 - partial_sums**2 / weights
+            costs = np.sum(cost_matrix, axis=1)
+            return costs
+
+        self.jitted_compute = l2_cost
+
+    def _build_jitted_compute_generic(self):
+        # Slower, but useful for flexibly building other components on top.
+        @njit(cache=True)
+        def l2_cost(x: np.ndarray) -> float:
+            n = x.shape[0]
+            sum2 = np.sum(x**2, axis=0)
+            sum = np.sum(x, axis=0)
+            univar_costs = sum2 - sum**2 / n
+            cost = np.sum(univar_costs)
+            return cost
+
+        self.jitted_compute_generic = l2_cost
+
+    def _build_jitted_compute_fixed(self):
+        # Needed for building savings automatically from costs.
+        mean = self._fixed_mean
+
+        @njit(cache=True)
+        def fixed_l2_cost(x: np.ndarray, mean: float | np.ndarray = mean) -> np.ndarray:
+            univar_costs = np.sum((x - mean) ** 2, axis=0)
+            cost = np.sum(univar_costs)
+            return cost
+
+        self.jitted_compute_fixed = fixed_l2_cost
+
+
+class L2CostOld(BaseCostOld):
     """L2 cost function."""
 
     def _build_jitted_precompute(self):
@@ -34,3 +136,36 @@ class L2Cost(BaseCost):
             return costs
 
         self.jitted_compute = l2_cost
+
+
+class EstimatedL2Cost(BaseCostOld):
+    """L2 cost function."""
+
+    def _build_jitted_compute(self):
+        @njit(cache=True)
+        def l2_cost(x: np.ndarray) -> np.ndarray:
+            n = x.shape[0]
+            cost_matrix = n * np.var(x, axis=0)
+            cost = np.sum(cost_matrix, axis=1)
+            return cost
+
+        self.jitted_compute = l2_cost
+
+
+class FixedL2Cost(BaseCostOld):
+    """L2 cost for a fixed mean."""
+
+    def __init__(self, mean: float | np.ndarray):
+        self.mean = mean
+        self._mean = np.asarray(mean).reshape(-1)
+
+    def _build_jitted_compute(self):
+        mean = self._mean
+
+        @njit(cache=True)
+        def fixed_l2_cost(x: np.ndarray) -> np.ndarray:
+            cost_matrix = np.sum((x - mean) ** 2, axis=0)
+            cost = np.sum(cost_matrix, axis=1)
+            return cost
+
+        self.jitted_compute = fixed_l2_cost
