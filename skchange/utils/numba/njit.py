037929b9d63ce9bc4de370b38db38a963b7b398c
tveten <tveten@nr.no>
2024-11-10 01:00:28 +0100

    fix: njit_configured

--- skchange/utils/numba/njit.py
+++ skchange/utils/numba/njit.py
@@ -7,15 +7,8 @@ from skchange.config import get_config
 if _check_soft_dependencies("numba", severity="ignore") and get_config("use_njit"):
     from numba import jit, njit  # noqa E402
 
-    def jit_configured():
-        """Jit dectorator with configured arguments."""
-        njit_args = get_config("njit_args")
-        return jit(**njit_args)
-
-    def njit_configured():
-        """Define njit dectorator with configured arguments."""
-        njit_args = get_config("njit_args")
-        return njit(**njit_args)
+    jit_configured = jit(**get_config("njit_args"))
+    njit_configured = njit(**get_config("njit_args"))
 
 else:
 
@@ -30,10 +23,10 @@ else:
         """Identity decorator for replacing njit by passthrough."""
         return _identity
 
-    def jit_configured():
-        """Identity decorator for replacing configured_jit by passthrough."""
+    def jit_configured(*args, **kwargs):
+        """Identity decorator for replacing jit_configured by passthrough."""
         return _identity
 
-    def njit_configured():
-        """Identity decorator for replacing configured_njit by passthrough."""
+    def njit_configured(*args, **kwargs):
+        """Identity decorator for replacing njit_configured by passthrough."""
         return _identity
