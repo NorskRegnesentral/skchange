c1e29d38a6ca1bc5eb05bb9f8b3b2815d3dd1fc9
tveten <tveten@nr.no>
2024-11-12 22:28:19 +0100

    add exploration of cost inside pelt

--- interactive/explore_evaluators.py
+++ interactive/explore_evaluators.py
@@ -1,5 +1,9 @@
 """Interactive exploration of the evaluators module."""
 
+import numpy as np
+import pandas as pd
+
+from skchange.change_detectors.pelt import Pelt
 from skchange.datasets.generate import generate_alternating_data
 from skchange.interval_evaluators.costs import L2Cost
 
@@ -18,3 +22,13 @@ cost.fit(df)
 
 intervals = generate_all_intervals(df)
 cost.evaluate(intervals)
+
+# Test PELT
+df = generate_alternating_data(
+    n_segments=5, mean=10, segment_length=100000, p=1, random_state=2
+)
+n = 10000
+df = pd.DataFrame(np.random.normal(0, 1, size=n))
+cost = L2Cost()
+detector = Pelt(cost=cost)
+detector.fit_predict(df)
