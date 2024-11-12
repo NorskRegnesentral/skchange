089150b5f8104b7845a97ffcc9b63c001af16f75
tveten <tveten@nr.no>
2024-11-12 22:29:27 +0100

    feat(api): remove config
    It will be done properly in another fashion in another PR.

--- skchange/utils/numba/njit.py
+++ skchange/utils/numba/njit.py
@@ -2,31 +2,17 @@
 
 from sktime.utils.dependencies import _check_soft_dependencies
 
-from skchange.config import config
-
-if _check_soft_dependencies("numba", severity="ignore") and config.enable_numba:
+if _check_soft_dependencies("numba", severity="ignore"):
     from numba import jit, njit  # noqa E402
-
-    jit_configured = jit(**config.njit_args)
-    njit_configured = njit(**config.njit_args)
-
 else:
 
-    def _identity(func):
-        return func
+    def identity_decorator_factory(*args, **kwargs):
+        """Make an identity decorator."""
 
-    def jit(*args, **kwargs):
-        """Identity decorator for replacing jit by passthrough."""
+        def _identity(func):
+            return func
+
         return _identity
 
-    def njit(*args, **kwargs):
-        """Identity decorator for replacing njit by passthrough."""
-        return _identity
-
-    def jit_configured(*args, **kwargs):
-        """Identity decorator for replacing jit_configured by passthrough."""
-        return _identity
-
-    def njit_configured(*args, **kwargs):
-        """Identity decorator for replacing njit_configured by passthrough."""
-        return _identity
+    jit = identity_decorator_factory
+    njit = identity_decorator_factory
