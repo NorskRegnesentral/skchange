e2bd0b6e57d840d889724d439afe614c8f2cb6c7
tveten <tveten@nr.no>
2024-11-13 23:23:00 +0100

    feat(api): make a base folder to put the most fundamental base objects
    Puts base detector and base interval evaluator in the same spot.
    
    Makes it easier to split the different evaluators into costs, change_scores and anomaly_scores modules inside the skchange folder. Less nested than having them inside an interval_evaluator folder.

---
+++ skchange/base/__init__.py
@@ -0,0 +1,6 @@
+"""Base classes for detectors and other objects in skchange."""
+
+from skchange.base.base_detector import BaseDetector
+from skchange.base.base_interval_evaluator import BaseIntervalEvaluator
+
+__all__ = ["BaseDetector", "BaseIntervalEvaluator"]
