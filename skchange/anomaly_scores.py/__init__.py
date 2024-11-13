63027dfb3f4cabd6424e94c99547afb28488c702
tveten <tveten@nr.no>
2024-11-13 23:36:56 +0100

    refactor: make an anomaly_scores module containing savings

---
+++ skchange/anomaly_scores.py/__init__.py
@@ -0,0 +1,1 @@
+"""Anomaly scores for interval evaluation."""
--- skchange/anomaly_scores.py/savings.py
+++ skchange/anomaly_scores.py/savings.py
@@ -1,33 +1,10 @@
-"""Module for calculating anomaly scores."""
+"""Saving-type anomaly scores."""
 
 import numpy as np
 from numpy.typing import ArrayLike
 
-from skchange.interval_evaluators.base import BaseIntervalEvaluator
+from skchange.anomaly_scores.base import BaseSaving
 from skchange.interval_evaluators.costs import BaseCost
-from skchange.interval_evaluators.utils import check_array_intervals
-
-
-class BaseSaving(BaseIntervalEvaluator):
-    """Base class template for savings.
-
-    A saving is a measure of the difference between a cost with a fixed baseline
-    parameter and an optimised cost over an interval. Most commonly, the baseline
-    parameter is pre-calculated robustly over the entire dataset under the assumption
-    that anomalies are rare. Each saving thus represents the potential cost reduction if
-    the parameter was optimised for the interval.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    @property
-    def min_size(self) -> int:
-        """Minimum size of the interval to evaluate."""
-        return 1
-
-    def _check_intervals(self, intervals: ArrayLike) -> np.ndarray:
-        return check_array_intervals(intervals, min_size=self.min_size, last_dim_size=2)
 
 
 class Saving(BaseSaving):
