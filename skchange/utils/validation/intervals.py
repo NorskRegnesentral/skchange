0098f64e0794009ec87b80b9f9b1574a66b7cb99
tveten <tveten@nr.no>
2024-11-14 23:52:09 +0100

    remove modifications of intervals in check_array_intervals + docs

--- skchange/utils/validation/intervals.py
+++ skchange/utils/validation/intervals.py
@@ -1,18 +1,36 @@
 """Utility functions for interval evaluators."""
 
 import numpy as np
-from numpy.typing import ArrayLike
-
-from skchange.utils.validation.data import as_2d_array
 
 
 def check_array_intervals(
-    intervals: ArrayLike,
-    min_size: int,
-    last_dim_size: int,
+    intervals: np.ndarray,
+    min_size: int = 1,
+    last_dim_size: int = 2,
 ) -> np.ndarray:
-    """Check array type intervals."""
-    intervals = as_2d_array(intervals, vector_as_column=False)
+    """Check array type intervals.
+
+    Parameters
+    ----------
+    intervals : np.ndarray
+        Array of intervals to check.
+    min_size : int, optional (default=1)
+        Minimum size of the intervals.
+    last_dim_size : int, optional (default=2)
+        Size of the last dimension.
+
+    Returns
+    -------
+    intervals : np.ndarray
+        The unmodified input intervals array.
+
+    Raises
+    ------
+    ValueError
+        If the intervals does not meet the requirements.
+    """
+    if intervals.ndim != 2:
+        raise ValueError("The intervals must be a 2D array.")
 
     if not np.issubdtype(intervals.dtype, np.integer):
         raise ValueError("The intervals must be of integer type.")
