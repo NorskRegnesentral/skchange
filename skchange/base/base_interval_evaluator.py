686cab2fe8c072c56e365c8c51afb24ce9b1acb7
tveten <tveten@nr.no>
2024-11-14 23:53:09 +0100

    docs: document BaseIntervalEvaluator

--- skchange/base/base_interval_evaluator.py
+++ skchange/base/base_interval_evaluator.py
@@ -17,14 +17,18 @@ Recommended but optional to implement for a concrete detector:
 __author__ = ["Tveten"]
 __all__ = ["BaseIntervalEvaluator"]
 
+import numpy as np
+from numpy.typing import ArrayLike
 from sktime.base import BaseEstimator
 from sktime.utils.validation.series import check_series
+
+from skchange.utils.validation.data import as_2d_array
 
 
 class BaseIntervalEvaluator(BaseEstimator):
     """Base class template for interval evaluators.
 
-    Interval evaluators are used to evaluate a function on given intervals of data.
+    This is a common base class for costs, change scores and anomaly scores.
     """
 
     def __init__(self):
@@ -83,9 +87,38 @@ class BaseIntervalEvaluator(BaseEstimator):
         """
         return self
 
-    def evaluate(self, intervals) -> float:
-        """Evaluate on a set of intervals."""
+    def evaluate(self, intervals: ArrayLike) -> np.ndarray:
+        """Evaluate on a set of intervals.
+
+        Parameters
+        ----------
+        intervals : ArrayLike
+            Integer location based intervals to evaluate. If intervals is a 2D array,
+            the subsets X[intervals[i, 0]:intervals[i, -1]] for
+            i = 0, ..., len(intervals) are evaluated.
+            If intervals is a 1D array, it is assumed to represent a single interval.
+
+            If intervals contains additional columns, these represent splitting points
+            within the interval, and needs to be in increasing order.
+            If splitting points are given, the same slicing convention is used for the
+            intervals between the splitting points:
+            [start, split_1), [split_1, split_2), ..., [split_n, end).
+
+        Returns
+        -------
+        values : np.ndarray
+            One value for each interval.
+
+        Notes
+        -----
+        Different interval evaluators may require different number of columns in the
+        intervals array. For example, a cost function requires two columns, one for the
+        start of the interval and one for the end of the interval. A change score
+        requires three columns, one for the start of the interval, one for the
+        splitting point within the interval, and one for the end of the interval.
+        """
         self.check_is_fitted()
+        intervals = as_2d_array(intervals, vector_as_column=False)
         intervals = self._check_intervals(intervals)
 
         values = self._evaluate(intervals)
@@ -95,9 +128,56 @@ class BaseIntervalEvaluator(BaseEstimator):
 
         return values
 
-    def _evaluate(self, intervals) -> float:
+    def _evaluate(self, intervals: np.ndarray) -> np.ndarray:
+        """Evaluate on a set of intervals.
+
+        The core logic of evaluating a function on a set of intervals is implemented
+        here.
+
+        Parameters
+        ----------
+        intervals : np.ndarray
+            A 2D array of integer location based intervals to evaluate.
+            the subsets X[intervals[i, 0]:intervals[i, -1]] for
+            i = 0, ..., len(intervals) are evaluated.
+
+            If intervals contains additional columns, these represent splitting points
+            within the interval, and needs to be in increasing order.
+            If splitting points are given, the same slicing convention is used for the
+            intervals between the splitting points:
+            [start, split_1), [split_1, split_2), ..., [split_n, end).
+
+        Returns
+        -------
+        values : np.ndarray
+            One value for each interval.
+        """
         raise NotImplementedError("abstract method")
 
-    def _check_intervals(self, intervals):
-        """Check intervals for compatibility."""
+    def _check_intervals(self, intervals: np.ndarray) -> np.ndarray:
+        """Check intervals for compatibility.
+
+        Parameters
+        ----------
+        intervals : np.ndarray
+            A 2D array of integer location based intervals to evaluate.
+            the subsets X[intervals[i, 0]:intervals[i, -1]] for
+            i = 0, ..., len(intervals) are evaluated.
+
+            If intervals contains additional columns, these represent splitting points
+            within the interval, and needs to be in increasing order.
+            If splitting points are given, the same slicing convention is used for the
+            intervals between the splitting points:
+            [start, split_1), [split_1, split_2), ..., [split_n, end).
+
+        Returns
+        -------
+        intervals : np.ndarray
+            The unmodified input intervals array.
+
+        Raises
+        ------
+        ValueError
+            If the intervals are not compatible.
+        """
         return intervals
