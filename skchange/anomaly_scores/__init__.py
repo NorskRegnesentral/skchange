0a4d950dbb958d82c36fb667c76fa60ed8cac413
tveten <tveten@nr.no>
2024-11-16 23:27:30 +0100

    feat: add lists in __init__ of all sub interval evaluators
    Useful for imports, as well as in automated testing. May want to automate this lookup at some point

--- skchange/anomaly_scores/__init__.py
+++ skchange/anomaly_scores/__init__.py
@@ -1,1 +1,13 @@
 """Anomaly scores for interval evaluation."""
+
+from skchange.anomaly_scores.base import BaseSaving
+from skchange.anomaly_scores.cost_based import Saving
+
+BASE_ANOMALY_SCORES = [
+    BaseSaving,
+]
+ANOMALY_SCORES = [
+    Saving,
+]
+
+__all__ = BASE_ANOMALY_SCORES + ANOMALY_SCORES
--- skchange/change_scores/__init__.py
+++ skchange/change_scores/__init__.py
@@ -1,1 +1,13 @@
 """Change scores as interval evaluators."""
+
+from skchange.change_scores.base import BaseChangeScore
+from skchange.change_scores.cost_based import ChangeScore
+
+BASE_CHANGE_SCORES = [
+    BaseChangeScore,
+]
+CHANGE_SCORES = [
+    ChangeScore,
+]
+
+__all__ = BASE_CHANGE_SCORES + CHANGE_SCORES
