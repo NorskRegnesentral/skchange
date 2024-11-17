03c697318bcbb018eb4e7d62f6930baf4b67067a
tveten <tveten@nr.no>
2024-11-17 16:16:05 +0100

    fix: print error messages without tuples

--- skchange/utils/validation/intervals.py
+++ skchange/utils/validation/intervals.py
@@ -37,28 +37,22 @@ def check_array_intervals(
 
     if intervals.shape[-1] != last_dim_size:
         raise ValueError(
-            (
-                "The intervals must be specified as an array with length",
-                f" {last_dim_size} in the last dimension.",
-            )
+            "The intervals must be specified as an array with length "
+            f"{last_dim_size} in the last dimension."
         )
 
     if not np.all(np.diff(intervals, axis=1) > 0):
         raise ValueError(
-            (
-                "All rows in the intervals must be strictly increasing.",
-                "The first and last columns represent the starts and ends of the",
-                "intervals respectively.",
-            )
+            "All rows in the intervals must be strictly increasing."
+            "The first and last columns represent the starts and ends of the"
+            "intervals respectively."
         )
 
     interval_sizes = intervals[:, -1] - intervals[:, 0]
     if np.any(interval_sizes < min_size):
         raise ValueError(
-            (
-                f"The interval sizes must be at least {min_size}."
-                f" Found an interval with size {np.min(interval_sizes)}.",
-            )
+            f"The interval sizes must be at least {min_size}."
+            f" Found an interval with size {np.min(interval_sizes)}."
         )
 
     return intervals
