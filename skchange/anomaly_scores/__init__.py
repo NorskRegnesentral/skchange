95c34128a4f8e848aabd7a9f15934055df2986a4
tveten <tveten@nr.no>
2024-11-21 01:32:34 +0100

    feat: add list of savings to __init__
    Needed for testing methods that use savings

--- skchange/anomaly_scores/__init__.py
+++ skchange/anomaly_scores/__init__.py
@@ -3,12 +3,15 @@
 from skchange.anomaly_scores.base import BaseSaving
 from skchange.anomaly_scores.from_cost import Saving, to_saving
 
-BASE_ANOMALY_SCORES = [
+BASE_SAVINGS = [
     BaseSaving,
 ]
-ANOMALY_SCORES = [
+SAVINGS = [
     Saving,
 ]
+BASE_ANOMALY_SCORES = BASE_SAVINGS
+ANOMALY_SCORES = SAVINGS
+
 
 __all__ = (
     BASE_ANOMALY_SCORES
