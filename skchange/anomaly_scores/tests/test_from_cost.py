27fa0500e6e35169df42fdc64a11758f77bd0525
tveten <tveten@nr.no>
2024-11-21 23:17:13 +0100

    tests: import fixed param finder from cost tests

--- skchange/anomaly_scores/tests/test_from_cost.py
+++ skchange/anomaly_scores/tests/test_from_cost.py
@@ -3,34 +3,7 @@ import pytest
 
 from skchange.anomaly_scores.from_cost import Saving, to_saving
 from skchange.costs import COSTS
-
-
-def find_fixed_param_combinations(cost_class):
-    """Find the first fixed parameter combination in the test parameters of a cost."""
-    test_param_sets = cost_class.get_test_params()
-    fixed_test_param_set = None
-    for param_set in test_param_sets:
-        if "param" in param_set and param_set["param"] is not None:
-            fixed_test_param_set = param_set
-            break
-
-    if fixed_test_param_set is None:
-        raise ValueError(
-            f"No fixed `param` argument found in `get_test_params()` of"
-            f" the cost class {cost_class.__name__}"
-        )
-
-    return fixed_test_param_set
-
-
-def test_find_fixed_param_combinations_value_error():
-    class MockCost:
-        @staticmethod
-        def get_test_params():
-            return [{"param": None}, {"param": None}]
-
-    with pytest.raises(ValueError):
-        find_fixed_param_combinations(MockCost)
+from skchange.costs.tests.test_all_costs import find_fixed_param_combinations
 
 
 @pytest.mark.parametrize("cost_class", COSTS)
