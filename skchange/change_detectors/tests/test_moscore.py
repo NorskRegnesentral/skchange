9a2bab0c368e0792871509561f0917f87f0c74d4
tveten <tveten@nr.no>
2024-11-21 01:23:05 +0100

    tests: update moscore tests

--- skchange/change_detectors/tests/test_moscore.py
+++ skchange/change_detectors/tests/test_moscore.py
@@ -4,45 +4,45 @@ import numpy as np
 import pytest
 
 from skchange.change_detectors.moscore import Moscore
+from skchange.change_scores import CHANGE_SCORES
 from skchange.datasets.generate import generate_alternating_data
-from skchange.scores.score_factory import VALID_CHANGE_SCORES
 
 
-@pytest.mark.parametrize("score", VALID_CHANGE_SCORES)
-def test_moscore_changepoint(score):
+@pytest.mark.parametrize("Score", CHANGE_SCORES)
+def test_moscore_changepoint(Score):
     """Test Moscore changepoints."""
     n_segments = 2
     seg_len = 50
     df = generate_alternating_data(
         n_segments=n_segments, mean=15, segment_length=seg_len, p=1, random_state=2
     )
-    detector = Moscore(score)
+    detector = Moscore(Score())
     changepoints = detector.fit_predict(df)
     assert len(changepoints) == n_segments - 1 and changepoints[0] == seg_len - 1
 
 
-@pytest.mark.parametrize("score", VALID_CHANGE_SCORES)
-def test_moscore_scores(score):
+@pytest.mark.parametrize("Score", CHANGE_SCORES)
+def test_moscore_scores(Score):
     """Test Moscore scores."""
     n_segments = 2
     seg_len = 50
     df = generate_alternating_data(
         n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=3
     )
-    detector = Moscore(score)
+    detector = Moscore(Score())
     scores = detector.fit(df).score_transform(df)
     assert np.all(scores >= 0.0)
     assert len(scores) == len(df)
 
 
-@pytest.mark.parametrize("score", VALID_CHANGE_SCORES)
-def test_moscore_tuning(score):
+@pytest.mark.parametrize("Score", CHANGE_SCORES)
+def test_moscore_tuning(Score):
     """Test Moscore tuning."""
     n_segments = 2
     seg_len = 50
     df = generate_alternating_data(
         n_segments=n_segments, mean=10, segment_length=seg_len, p=1, random_state=4
     )
-    detector = Moscore(score, threshold_scale=None)
+    detector = Moscore(Score(), threshold_scale=None)
     detector.fit(df)
     assert detector.threshold_ > 0.0
