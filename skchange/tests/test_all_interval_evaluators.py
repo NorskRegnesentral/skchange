c05ca2ba65730f358d05e171abf8f1f1cd0123c8
tveten <tveten@nr.no>
2024-11-21 20:58:03 +0100

    tests: add more tests on evaluate intervals input

--- skchange/tests/test_all_interval_evaluators.py
+++ skchange/tests/test_all_interval_evaluators.py
@@ -28,11 +28,39 @@ def test_evaluator_evaluate(Evaluator):
     evaluator = Evaluator.create_test_instance()
     x = make_annotation_problem(n_timepoints=50, estimator_type="None")
     evaluator.fit(x)
-    evaluator.expected_interval_entries
     interval1 = np.linspace(0, 10, evaluator.expected_interval_entries, dtype=int)
+
+    results = evaluator.evaluate(interval1)
+    assert isinstance(results, np.ndarray)
+    assert results.ndim == 2
+    assert len(results) == 1
+
     interval2 = np.linspace(10, 20, evaluator.expected_interval_entries, dtype=int)
     intervals = np.array([interval1, interval2])
     results = evaluator.evaluate(intervals)
     assert isinstance(results, np.ndarray)
     assert results.ndim == 2
     assert len(results) == len(intervals)
+
+
+@pytest.mark.parametrize("Evaluator", INTERVAL_EVALUATORS)
+def test_evaluator_invalid_intervals(Evaluator):
+    evaluator = Evaluator.create_test_instance()
+    x = make_annotation_problem(n_timepoints=50, estimator_type="None")
+    evaluator.fit(x)
+    with pytest.raises(ValueError):
+        interval = np.linspace(0, 10, evaluator.expected_interval_entries, dtype=float)
+        evaluator.evaluate(interval)
+    with pytest.raises(ValueError):
+        interval = np.linspace(
+            0, 10, evaluator.expected_interval_entries - 1, dtype=int
+        )
+        evaluator.evaluate(interval)
+    with pytest.raises(ValueError):
+        interval = np.linspace(
+            0, 10, evaluator.expected_interval_entries + 1, dtype=int
+        )
+        evaluator.evaluate(interval)
+    with pytest.raises(ValueError):
+        interval = np.linspace(10, 0, evaluator.expected_interval_entries, dtype=int)
+        evaluator.evaluate(interval)
