0dd055165b70a83084c8c4034cb77d1968cf55f7
tveten <tveten@nr.no>
2024-11-23 01:17:00 +0100

    feat: add GaussianCovCost to init

--- skchange/costs/__init__.py
+++ skchange/costs/__init__.py
@@ -1,12 +1,14 @@
 """Cost functions for cost-based change and anomaly detection."""
 
 from skchange.costs.base import BaseCost
+from skchange.costs.gaussian_cov_cost import GaussianCovCost
 from skchange.costs.l2_cost import L2Cost
 
 BASE_COSTS = [
     BaseCost,
 ]
 COSTS = [
+    GaussianCovCost,
     L2Cost,
 ]
 
