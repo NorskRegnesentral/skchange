cf5d568cdac47038cbe1d27be77ca374a85a42cd
tveten <tveten@nr.no>
2024-11-24 20:38:16 +0100

    tests: add test for evaluate output by data_type

--- skchange/tests/test_all_interval_evaluators.py
+++ skchange/tests/test_all_interval_evaluators.py
@@ -6,6 +6,7 @@ from sktime.utils.estimator_checks import check_estimator, parametrize_with_chec
 from skchange.anomaly_scores import ANOMALY_SCORES
 from skchange.change_scores import CHANGE_SCORES
 from skchange.costs import COSTS
+from skchange.datasets import generate_alternating_data
 
 INTERVAL_EVALUATORS = COSTS + CHANGE_SCORES + ANOMALY_SCORES
 
@@ -41,6 +42,35 @@ def test_evaluator_evaluate(Evaluator):
     assert isinstance(results, np.ndarray)
     assert results.ndim == 2
     assert len(results) == len(intervals)
+
+
+@pytest.mark.parametrize("Evaluator", INTERVAL_EVALUATORS)
+def test_evaluator_evaluate_by_data_type(Evaluator):
+    evaluator = Evaluator.create_test_instance()
+    n_segments = 1
+    seg_len = 50
+    p = 3
+    df = generate_alternating_data(
+        n_segments=n_segments,
+        mean=20,
+        segment_length=seg_len,
+        p=p,
+        random_state=15,
+    )
+
+    evaluator.fit(df)
+    interval1 = np.linspace(0, 10, evaluator.expected_interval_entries, dtype=int)
+    interval2 = np.linspace(10, 20, evaluator.expected_interval_entries, dtype=int)
+    intervals = np.array([interval1, interval2])
+
+    results = evaluator.evaluate(intervals)
+
+    if evaluator.data_type == "univariate":
+        assert results.shape == (2, p)
+    elif evaluator.data_type == "multivariate":
+        assert results.shape == (2, 1)
+    else:
+        raise ValueError("Invalid scitype:evaluator tag.")
 
 
 @pytest.mark.parametrize("Evaluator", INTERVAL_EVALUATORS)
