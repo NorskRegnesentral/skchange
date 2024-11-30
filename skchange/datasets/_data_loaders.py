7afad2669d887f3bb033afb151644e369ce6bc4d
tveten <tveten@nr.no>
2024-11-30 21:38:53 +0100

    data: add a second air handling unit to the dataset

--- skchange/datasets/_data_loaders.py
+++ skchange/datasets/_data_loaders.py
@@ -5,25 +5,33 @@ import os
 import pandas as pd
 
 
-def load_air_handling_unit():
-    """Load the air handling unit dataset.
+def load_air_handling_units():
+    """Load the air handling units dataset.
 
-    The dataset contains a time series of vibration magnitude measurements from an
-    air handling unit monitored over a period of 30 days. The data is sampled every
-    10 minutes.
+    The dataset contains time series of vibration magnitude measurements from two
+    different air handling units, each monitored over a period of 30 days. The data is
+    sampled every 10 minutes.
+
+    The aim of analysing the data is to detect when the air handling units normally
+    turn on and off, such that anomalies from their regular schedule can be detected.
 
     The data has been provided by Soundsensing AS.
 
     Returns
     -------
     pd.DataFrame
-        The air handling unit dataset. The index is time, and there is one column with
-        the vibration magnitude measurements.
+        The air handling unit dataset. It has a multi-index with two levels:
+
+        1. "unit_id": A string identifier for each air handling unit.
+        2. "time": A datetime index with the time of each measurement.
+
+        There's one column:
+
+        1. "vibration": A float column with the vibration magnitude measurements.
     """
     this_file_dir = os.path.dirname(__file__)
-    file_path = os.path.join(
-        this_file_dir, "data", "air_handling_unit", "air_handling_unit.csv"
-    )
-    df = pd.read_csv(file_path)[1:].set_index("time")
-    df.index = pd.to_datetime(df.index)
+    file_path = os.path.join(this_file_dir, "data", "air_handling_unit", "data.csv")
+    df = pd.read_csv(file_path).iloc[:, 1:]
+    df["time"] = pd.to_datetime(df["time"])
+    df = df.set_index(["unit_id", "time"])
     return df
