10cf886e08569487b3035276296f4f92f461038a
tveten <tveten@nr.no>
2024-12-04 11:58:02 +0100

    release script: use make build

--- build_tools/make_release.py
+++ build_tools/make_release.py
@@ -204,7 +204,7 @@ class MakeDist(Step):
 
     def action(self, context):
         """Carry out action."""
-        self.do_cmd("make dist")
+        self.do_cmd("make build")
 
 
 class CheckDist(Step):
