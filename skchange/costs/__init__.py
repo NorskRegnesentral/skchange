c625b31fefb76ec66162ebb59e858734f73ff267
tveten <tveten@nr.no>
2024-12-11 16:05:04 +0100

    rename module to gaussian_cost.py

--- skchange/costs/__init__.py
+++ skchange/costs/__init__.py
@@ -1,7 +1,7 @@
 """Cost functions for cost-based change and anomaly detection."""
 
 from skchange.costs.base import BaseCost
-from skchange.costs.gaussian_var_cost import GaussianCost
+from skchange.costs.gaussian_cost import GaussianCost
 from skchange.costs.l2_cost import L2Cost
 from skchange.costs.multivariate_gaussian_cost import MultivariateGaussianCost
 
