1c121e65a671234977a5bb3439c0f304b3ad2981
tveten <tveten@nr.no>
2024-12-11 22:36:34 +0100

    tests: collective -> segment

--- skchange/anomaly_detectors/tests/test_anomaly_detectors.py
+++ skchange/anomaly_detectors/tests/test_anomaly_detectors.py
@@ -15,8 +15,8 @@ anomaly_free_data = generate_anomalous_data(100, random_state=1)
 
 
 @pytest.mark.parametrize("Estimator", COLLECTIVE_ANOMALY_DETECTORS)
-def test_collective_anomaly_detector_predict(Estimator: BaseSegmentAnomalyDetector):
-    """Test collective anomaly detector's predict method (sparse output)."""
+def test_segment_anomaly_detector_predict(Estimator: BaseSegmentAnomalyDetector):
+    """Test segment anomaly detector's predict method (sparse output)."""
     detector = Estimator.create_test_instance()
     detector.fit(anomaly_free_data)
     anomalies = detector.predict(anomaly_data)["ilocs"]
@@ -27,18 +27,18 @@ def test_collective_anomaly_detector_predict(Estimator: BaseSegmentAnomalyDetect
 
 
 @pytest.mark.parametrize("Estimator", COLLECTIVE_ANOMALY_DETECTORS)
-def test_collective_anomaly_detector_transform(
+def test_segment_anomaly_detector_transform(
     Estimator: BaseSegmentAnomalyDetector,
 ):
-    """Test collective anomaly detector's transform method (dense output)."""
+    """Test segment anomaly detector's transform method (dense output)."""
     detector = Estimator.create_test_instance()
     detector.fit(anomaly_free_data)
     labels = detector.transform(anomaly_data)
-    true_collective_anomalies = pd.DataFrame(
+    true_segment_anomalies = pd.DataFrame(
         {"ilocs": pd.IntervalIndex.from_tuples(true_anomalies, closed="left")}
     )
     true_anomaly_labels = BaseSegmentAnomalyDetector.sparse_to_dense(
-        true_collective_anomalies, anomaly_data.index
+        true_segment_anomalies, anomaly_data.index
     )
     labels.equals(true_anomaly_labels)
 
