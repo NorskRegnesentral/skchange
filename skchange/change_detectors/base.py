c49ff7ca6cfe369d60e53745209dc5886a2b77dd
tveten <tveten@nr.no>
2024-12-13 00:15:51 +0100

    docs: changepoint -> change point

--- skchange/change_detectors/base.py
+++ skchange/change_detectors/base.py
@@ -1,10 +1,10 @@
-"""Base classes for changepoint detectors.
+"""Base classes for change point detectors.
 
     classes:
         BaseChangeDetector
 
 By inheriting from these classes the remaining methods of the BaseDetector class to
-implement to obtain a fully functional changepoint detector are given below.
+implement to obtain a fully functional change point detector are given below.
 
 Needs to be implemented:
     _fit(self, X, y=None)
@@ -25,8 +25,8 @@ class BaseChangeDetector(BaseDetector):
     """Base class for change detectors.
 
     Changepoint detectors detect points in time where a change in the data occurs.
-    Data between two changepoints is a segment where the data is considered to be
-    homogeneous, i.e., of the same distribution. A changepoint is defined as the
+    Data between two change points is a segment where the data is considered to be
+    homogeneous, i.e., of the same distribution. A change point is defined as the
     location of the first element of a segment.
     """
 
@@ -45,7 +45,7 @@ class BaseChangeDetector(BaseDetector):
         Parameters
         ----------
         y_sparse : pd.DataFrame
-            The sparse output from a changepoint detector's `predict` method.
+            The sparse output from a change point detector's `predict` method.
         index : array-like
             Indices that are to be annotated according to `y_sparse`.
         columns: array-like
@@ -55,7 +55,7 @@ class BaseChangeDetector(BaseDetector):
         -------
         pd.DataFrame with the input data index and one column:
         * ``"label"`` - integer labels ``0, ..., K`` for each segment between two
-        changepoints.
+        change points.
         """
         changepoints = y_sparse["ilocs"].to_list()
         n = len(index)
@@ -75,13 +75,13 @@ class BaseChangeDetector(BaseDetector):
         Parameters
         ----------
         y_dense : pd.DataFrame
-            The dense output from a changepoint detector's `transform` method.
+            The dense output from a change point detector's `transform` method.
 
         Returns
         -------
         pd.DataFrame :
             A `pd.DataFrame` with a range index and one column:
-            * ``"ilocs"`` - integer locations of the changepoints.
+            * ``"ilocs"`` - integer locations of the change points.
         """
         is_changepoint = y_dense["labels"].diff().abs() > 0
         changepoints = y_dense.index[is_changepoint]
@@ -89,25 +89,25 @@ class BaseChangeDetector(BaseDetector):
 
     @staticmethod
     def _format_sparse_output_ilocs(changepoints) -> pd.DataFrame:
-        """Format the sparse output of changepoint detectors.
+        """Format the sparse output of change point detectors.
 
         Can be reused by subclasses to format the output of the `_predict` method.
 
         Parameters
         ----------
         changepoints : list
-            List of changepoint locations.
+            List of change point locations.
 
         Returns
         -------
         pd.DataFrame :
             A `pd.DataFrame` with a range index and one column:
-            * ``"ilocs"`` - integer locations of the changepoints.
+            * ``"ilocs"`` - integer locations of the change points.
         """
         return pd.DataFrame(changepoints, columns=["ilocs"], dtype="int64")
 
     def _format_sparse_output(self, changepoints) -> pd.DataFrame:
-        """Format the sparse output of changepoint detectors.
+        """Format the sparse output of change point detectors.
 
         Can be reused by subclasses to format the output of the `_predict` method.
 
@@ -120,6 +120,6 @@ class BaseChangeDetector(BaseDetector):
         -------
         pd.DataFrame :
             A `pd.DataFrame` with a range index and one column:
-            * ``"ilocs"`` - integer locations of the changepoints.
+            * ``"ilocs"`` - integer locations of the change points.
         """
         return self._format_sparse_output_ilocs(changepoints)
