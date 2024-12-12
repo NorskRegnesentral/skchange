39eee2af7140a141a6aca964c49c57645b995160
tveten <tveten@nr.no>
2024-12-13 00:37:44 +0100

    docs: update cusum docs

--- skchange/change_scores/cusum.py
+++ skchange/change_scores/cusum.py
@@ -97,8 +97,8 @@ class CUSUM(BaseChangeScore):
         ----------
         cuts : np.ndarray
             A 2D array with three columns of integer locations.
-            The first column is the start, the second is the split, and the third is
-            the end of the interval to evaluate.
+            The first column is the ``start``, the second is the ``split``, and the
+            third is the ``end`` of the interval to evaluate.
             The difference between subsets ``X[start:split]`` and ``X[split:end]`` is
             evaluated for each row in `cuts`.
 
