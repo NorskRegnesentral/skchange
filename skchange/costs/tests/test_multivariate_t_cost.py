27b146ccc5903b3d5d34caa5fafc96a4c587cd78
jvkolsto <jvkolsto@nr.no>
2025-01-20 16:47:44 +0100

    Fix code quality in the mv-T-cost test file.

--- skchange/costs/tests/test_multivariate_t_cost.py
+++ skchange/costs/tests/test_multivariate_t_cost.py
@@ -204,7 +204,7 @@ def _multivariate_t_log_likelihood_like_scipy(
     """Calculate the log likelihood of a multivariate t-distribution.
 
     Directly from the definition of the multivariate t-distribution.
-    Implementation taken from the scipy implementation of the multivariate t-distribution.
+    Mirroring the scipy implementation of the multivariate t-distribution.
     Source: https://en.wikipedia.org/wiki/Multivariate_t-distribution
 
     Parameters
@@ -327,9 +327,9 @@ def test_scale_matrix_mle(seed=4125):
         centered_samples, loc=np.zeros(p), shape=mle_scale_matrix, df=t_dof
     ).sum()
 
-    assert mle_scale_matrix_ll > true_scale_matrix_ll, (
-        "MLE log-likelihood is not maximal."
-    )
+    assert (
+        mle_scale_matrix_ll > true_scale_matrix_ll
+    ), "MLE log-likelihood is not maximal."
 
 
 def test_loo_scale_matrix_mle(seed=4125):
@@ -494,24 +494,24 @@ def test_iso_and_kurt_dof_estimates_on_gaussian_data():
     )
 
     mv_t_kurt_dof_est = _kurtosis_mv_t_dof_estimate(mv_t_samples)
-    assert np.isfinite(mv_t_kurt_dof_est) and (mv_t_kurt_dof_est > 0.0), (
-        "Kurtosis dof estimate should be finite on multivariate t samples."
-    )
+    assert np.isfinite(mv_t_kurt_dof_est) and (
+        mv_t_kurt_dof_est > 0.0
+    ), "Kurtosis dof estimate should be finite on multivariate t samples."
 
     mv_t_isotropic_dof_est = _isotropic_mv_t_dof_estimate(mv_t_samples)
-    assert np.isfinite(mv_t_isotropic_dof_est) and (mv_t_isotropic_dof_est > 0.0), (
-        "Isotropic dof estimate should be finite on multivariate t samples."
-    )
+    assert np.isfinite(mv_t_isotropic_dof_est) and (
+        mv_t_isotropic_dof_est > 0.0
+    ), "Isotropic dof estimate should be finite on multivariate t samples."
 
     normal_kurt_dof_est = _kurtosis_mv_t_dof_estimate(mv_normal_samples)
-    assert np.isposinf(normal_kurt_dof_est), (
-        "Kurtosis dof estimate should be infinite on Gaussian data."
-    )
+    assert np.isposinf(
+        normal_kurt_dof_est
+    ), "Kurtosis dof estimate should be infinite on Gaussian data."
 
     normal_isotropic_dof_est = _isotropic_mv_t_dof_estimate(mv_normal_samples)
-    assert np.isposinf(normal_isotropic_dof_est), (
-        "Isotropic dof estimate should be infinite on Gaussian data."
-    )
+    assert np.isposinf(
+        normal_isotropic_dof_est
+    ), "Isotropic dof estimate should be infinite on Gaussian data."
 
 
 def test_iterative_t_dof_estimate():
@@ -540,9 +540,9 @@ def test_iterative_t_dof_estimate():
         centered_samples, initial_dof=initial_t_dof_estimate
     )
     assert iterative_dof_estimate > 0, "Data-driven dof estimate should be positive."
-    assert np.abs(iterative_dof_estimate - t_dof) < 1.0, (
-        "Data-driven dof estimate is off."
-    )
+    assert (
+        np.abs(iterative_dof_estimate - t_dof) < 1.0
+    ), "Data-driven dof estimate is off."
 
 
 def test_loo_iterative_t_dof_estimate():
@@ -571,9 +571,9 @@ def test_loo_iterative_t_dof_estimate():
         centered_samples, initial_dof=initial_t_dof_estimate
     )
     assert loo_iterative_dof > 0, "LOO data-driven dof estimate should be positive."
-    assert np.abs(loo_iterative_dof - t_dof) < 0.15, (
-        "LOO data-driven dof estimate is off."
-    )
+    assert (
+        np.abs(loo_iterative_dof - t_dof) < 0.15
+    ), "LOO data-driven dof estimate is off."
 
 
 def test_iterative_dof_estimate_returns_inf_on_gaussian_data():
@@ -600,9 +600,9 @@ def test_iterative_dof_estimate_returns_inf_on_gaussian_data():
         max_iter=100,
     )
 
-    assert np.isposinf(iterative_dof_estimate), (
-        "Dof estimate should be infinite on Gaussian data."
-    )
+    assert np.isposinf(
+        iterative_dof_estimate
+    ), "Dof estimate should be infinite on Gaussian data."
 
 
 def test_loo_iterative_dof_estimate_returns_inf_on_gaussian_data():
@@ -628,9 +628,9 @@ def test_loo_iterative_dof_estimate_returns_inf_on_gaussian_data():
         max_iter=100,
     )
 
-    assert np.isposinf(loo_iterative_dof), (
-        "Dof estimate should be infinite on Gaussian data."
-    )
+    assert np.isposinf(
+        loo_iterative_dof
+    ), "Dof estimate should be infinite on Gaussian data."
 
 
 def test_MultiVariateTCost_with_PELT(
@@ -676,9 +676,9 @@ def test_MultiVariateTCost_with_PELT(
     print(f"Estimated dof: {mv_t_cost.dof_}")
 
     assert len(change_points) == 1, "Only one change point should be detected."
-    assert change_points.loc[0, "ilocs"] == n_samples, (
-        "Change point should be at the end of the first segment."
-    )
+    assert (
+        change_points.loc[0, "ilocs"] == n_samples
+    ), "Change point should be at the end of the first segment."
     assert np.isfinite(mv_t_cost.dof_), "Fitted dof should be finite."
 
 
@@ -713,7 +713,7 @@ def test_MultiVariateTCost_with_moving_window(
     print(f"Estimated dof: {t_cost.dof_}")
 
     assert len(change_points) == 1, "Only one change point should be detected."
-    assert change_points.loc[0, "ilocs"] == n_samples, (
-        "Change point should be at the end of the first segment."
-    )
+    assert (
+        change_points.loc[0, "ilocs"] == n_samples
+    ), "Change point should be at the end of the first segment."
     assert np.isfinite(t_cost.dof_), "Fitted dof should be finite."
