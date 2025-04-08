386b3a1afb20f4cade68805c180ce281ed9ca1ca
johannvk-Acer-Windows <johannesvk@protonmail.com>
2025-04-08 22:39:53 +0200

    Use 'set_params' on the PELT object to update penalty values. Very nifty!

--- skchange/change_detectors/_crops.py
+++ skchange/change_detectors/_crops.py
@@ -2,11 +2,14 @@
 
 import numpy as np
 
+from ..costs.base import BaseCost
+from ..penalties import ConstantPenalty
 from . import PELT
 
 
 def pelt_crops(
-    change_point_detector: PELT,
+    cost: BaseCost,
+    X: np.ndarray,
     min_penalty: float,
     max_penalty: float,
 ):
@@ -23,9 +26,24 @@ def pelt_crops(
     max_penalty : float
         The maximum penalty to use.
     """
-    # Want a Heap of some kind, so that I can pop 
+    # Want a Heap of some kind, so that I can pop
     # and push intervals onto it.
     penalty_intervals = [
         (min_penalty, max_penalty),
     ]
+    change_point_detector = PELT(cost=cost)
+    change_point_detector.fit(X)
+    while len(penalty_intervals) > 0:
+        # Pop the interval with the lowest penalty.
+        min_penalty, max_penalty = penalty_intervals.pop(0)
+
+        # Run the change point detector on the interval.
+        # Need to update penalty for the PELT-detector:
+        min_penalty_change_points = change_point_detector.set_params(
+            penalty=min_penalty
+        ).predict(X)
+        max_penalty_change_points = change_point_detector.set_params(
+            penalty=max_penalty
+        ).predict(X)
+
     pass
