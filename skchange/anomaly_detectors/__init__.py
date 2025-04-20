6335df64ffa06f5ac7df7dc88cffc52546c99f66
tveten <tveten@nr.no>
2025-04-20 19:55:17 +0200

    feat(api)!: remove MVCAPA from anomaly_detectors init

--- skchange/anomaly_detectors/__init__.py
+++ skchange/anomaly_detectors/__init__.py
@@ -3,12 +3,10 @@
 from ._anomalisers import StatThresholdAnomaliser
 from ._capa import CAPA
 from ._circular_binseg import CircularBinarySegmentation
-from ._mvcapa import MVCAPA
 
 COLLECTIVE_ANOMALY_DETECTORS = [
     CAPA,
     CircularBinarySegmentation,
-    MVCAPA,
     StatThresholdAnomaliser,
 ]
 ANOMALY_DETECTORS = COLLECTIVE_ANOMALY_DETECTORS
