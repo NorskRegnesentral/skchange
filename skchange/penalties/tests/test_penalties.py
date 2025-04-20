9ab581160150b82c7c7f04dfbc3546e5439c388c
tveten <tveten@nr.no>
2025-04-20 19:13:22 +0200

    tests: update tests for penalties

--- skchange/penalties/tests/test_penalties.py
+++ skchange/penalties/tests/test_penalties.py
@@ -1,66 +1,88 @@
 """Tests for all penalties."""
 
 import numpy as np
-import pandas as pd
 import pytest
 
-from skchange.change_scores import CUSUM
-from skchange.penalties import PENALTIES
-from skchange.penalties.base import BasePenalty
-
-df = pd.DataFrame(np.random.randn(100, 3))
-scorer = CUSUM()
-
-
-@pytest.mark.parametrize("Penalty", PENALTIES)
-def test_penalty_type(Penalty: BasePenalty):
-    penalty = Penalty.create_test_instance()
-    supported_penalty_types = ["constant", "linear", "nonlinear"]
-    assert penalty.penalty_type in supported_penalty_types
+from skchange.penalties import (
+    make_bic_penalty,
+    make_chi2_penalty,
+    make_linear_chi2_penalty,
+    make_linear_penalty,
+    make_nonlinear_chi2_penalty,
+)
 
 
-@pytest.mark.parametrize("Penalty", PENALTIES)
-def test_fit(Penalty: BasePenalty):
-    penalty = Penalty.create_test_instance()
-    penalty.fit(df, scorer)
-    assert penalty._is_fitted
-
-
-@pytest.mark.parametrize("Penalty", PENALTIES)
-def test_values(Penalty: BasePenalty):
-    penalty = Penalty.create_test_instance()
-    penalty.fit(df, scorer)
-
-    assert isinstance(penalty.values, np.ndarray)
-
-    if penalty.penalty_type == "constant":
-        assert penalty.values.shape == (1,)
-    else:
-        assert penalty.values.shape == (penalty.p_,)
-
-    # Penalties can have value = 0, but the test instances should have positive values.
-    assert np.all(penalty.values > 0.0)
-    assert np.all(np.diff(penalty.values) >= 0)
-
-
-@pytest.mark.parametrize("Penalty", PENALTIES)
-def test_values_p1(Penalty: BasePenalty):
-    penalty = Penalty.create_test_instance()
-    df_1 = pd.DataFrame(np.random.randn(100, 1))
-    penalty.fit(df_1, scorer)
-
-    assert isinstance(penalty.values, np.ndarray)
-    assert penalty.values.shape == (1,)
-    # Penalties can have value = 0, but the test instances should have positive values.
-    assert np.all(penalty.values > 0.0)
-    assert np.all(np.diff(penalty.values) >= 0)
-
-
-@pytest.mark.parametrize("Penalty", PENALTIES)
-def test_scale(Penalty: BasePenalty):
-    penalty = Penalty.create_test_instance()
-    assert isinstance(penalty.scale, (int, float))
-    assert penalty.scale > 0
+def test_make_bic_penalty():
+    penalty = make_bic_penalty(2, 100)
+    assert isinstance(penalty, float) or (
+        isinstance(penalty, np.ndarray) and penalty.size == 1
+    )
+    assert penalty >= 0.0
 
     with pytest.raises(ValueError):
-        penalty.set_params(scale=-1.0)
+        make_bic_penalty(0, 100, 1)
+    with pytest.raises(ValueError):
+        make_bic_penalty(2, 0, 1)
+    with pytest.raises(ValueError):
+        make_bic_penalty(2, 100, -1)
+
+
+def test_make_chi2_penalty():
+    penalty = make_chi2_penalty(4, 20)
+    assert isinstance(penalty, float) or (
+        isinstance(penalty, np.ndarray) and penalty.size == 1
+    )
+    assert penalty >= 0.0
+
+    with pytest.raises(ValueError):
+        make_chi2_penalty(0, 100)
+    with pytest.raises(ValueError):
+        make_chi2_penalty(2, 0)
+
+
+def test_make_linear_chi2_penalty():
+    p = 4
+    penalty = make_linear_chi2_penalty(3, 100, p)
+    assert isinstance(penalty, np.ndarray)
+    assert penalty.shape == (p,)
+    assert np.all(penalty >= 0)
+    assert np.all(np.diff(penalty) >= 0)
+
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(0, 1, 1)
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(1, 0, 1)
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(1, 1, 0)
+
+
+def test_make_linear_penalty():
+    p = 10
+    penalty = make_linear_penalty(10, 5, p)
+    assert isinstance(penalty, np.ndarray)
+    assert penalty.shape == (p,)
+    assert np.all(penalty >= 0)
+    assert np.all(np.diff(penalty) >= 0)
+
+    with pytest.raises(ValueError):
+        make_linear_penalty(-1, 1, 1)
+    with pytest.raises(ValueError):
+        make_linear_penalty(1, -1, 1)
+    with pytest.raises(ValueError):
+        make_linear_penalty(1, 1, 0)
+
+
+def test_make_nonlinear_chi2_penalty():
+    p = 100
+    penalty = make_nonlinear_chi2_penalty(10, 200, p)
+    assert isinstance(penalty, np.ndarray)
+    assert penalty.shape == (p,)
+    assert np.all(penalty >= 0)
+    assert np.all(np.diff(penalty) >= 0)
+
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(0, 1, 1)
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(1, 0, 1)
+    with pytest.raises(ValueError):
+        make_linear_chi2_penalty(1, 1, 0)
