355de613415fb28f982ca60f456dda05409f94a4
tveten <tveten@nr.no>
2025-04-23 13:16:58 +0200

    docs: document evaluation type enum

--- skchange/utils/validation/enums.py
+++ skchange/utils/validation/enums.py
@@ -4,7 +4,16 @@ from enum import Enum
 
 
 class EvaluationType(Enum):
-    """Different types of evaluation for the change point detection algorithms."""
+    """Different types of evaluation for the change point detection algorithms.
+
+    * Univariate: Each variable is evaluated independently. The output of scorers have
+      the same number of columns as the input data.
+    * Multivariate: All variables are evaluated together. The output of scorers only
+      has one column.
+    * Conditional: Some variables are used as a response and others as covariates in a
+      regression model. The output of scorers have the same number of columns as the
+      number of response variables.
+    """
 
     UNIVARIATE = 1
     MULTIVARIATE = 2
