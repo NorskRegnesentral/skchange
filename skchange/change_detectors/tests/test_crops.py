73129edba5e93c1978a704bbf44e4b8468e21785
johannvk-Acer-Windows <johannesvk@protonmail.com>
2025-05-01 11:23:46 +0200

    Removed 'SeededBinarySegmentation' test in combination with CROPS

--- skchange/change_detectors/tests/test_crops.py
+++ skchange/change_detectors/tests/test_crops.py
@@ -35,34 +35,3 @@ def test_pelt_crops():
     results = change_point_detector.run_crops(dataset.values)
     # Check that the results are as expected:
     assert len(results) == 64
-
-
-def test_generic_crops_on_SeededBinarySegmentation():
-    cost = GaussianCost()
-    min_penalty = 0.1
-    max_penalty = 100.0
-    change_detector = SeededBinarySegmentation(change_score=to_change_score(cost))
-    # Initialize penalization interval change point detector:
-    crops_change_detector = GenericCROPS(
-        change_detector=change_detector,
-        segmentation_cost=cost,
-        min_penalty=min_penalty,
-        max_penalty=max_penalty,
-    )
-
-    # Generate test data:
-    dataset = generate_alternating_data(
-        n_segments=2,
-        segment_length=100,
-        p=1,
-        mean=3.0,
-        variance=4.0,
-        random_state=42,
-    )
-
-    # Fit the change point detector:
-    crops_change_detector.fit(dataset)
-    results = crops_change_detector.predict(dataset.values)
-    # Check that the results are as expected:
-    assert results is not None
-
