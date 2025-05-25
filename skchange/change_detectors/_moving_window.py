Fix off-by-one bug in MvGaussianScore, and bug in MovingWindow change detector when selecting changepoints by local optimum, and a candidate change point < selection bandwidth.

skchange/change_detectors/_moving_window.py
@@ -103,9 +103,14 @@ def select_changepoints_by_local_optimum(
         for cpt in candidate_cpts
         if np.isclose(
             scores[cpt],
-            np.max(scores[cpt - selection_bandwidth : cpt + selection_bandwidth + 1]),
+            np.max(
+                scores[
+                    max(cpt - selection_bandwidth, 0) : cpt + selection_bandwidth + 1
+                ]
+            ),
         )
     ]
+
     return cpts
 
 

skchange/change_scores/_multivariate_gaussian_score.py
@@ -77,10 +77,10 @@ def likelihood_ratio_expected_value(
 
     assert 0 < k < n, "Cut point `k` must be within the sequence length `n`."
     assert p > 0, "Dimension `p` must be a positive integer."
-    assert k > (p + 1), "Cut point `k` must be larger than the dimension + 1."
-    assert n - k > (
-        p + 1
-    ), "Run length after cut point `n - k` must be larger than dimension + 1."
+    assert k > p, "Cut point `k` must be larger than the dimension `p`."
+    assert n - k > p, (
+        "Run length after cut point `n - k` must be larger than dimension `p`."
+    )
 
     g_k_n = p * (
         np.log(2)