c5dce6b661fdbae10b73b72fe8b1417b42230f4c
tveten <tveten@nr.no>
2025-08-11 13:02:11 +0200

    refactor(data gen): separate data generators into different modules

--- skchange/datasets/__init__.py
+++ skchange/datasets/__init__.py
@@ -2,11 +2,13 @@
 
 from ._data_loaders import load_hvac_system_data
 from ._generate import (
+    generate_piecewise_data,
+)
+from ._generate_linear_trend import generate_continuous_piecewise_linear_signal
+from ._generate_normal import (
     generate_alternating_data,
     generate_anomalous_data,
     generate_changing_data,
-    generate_continuous_piecewise_linear_signal,
-    generate_piecewise_data,
     generate_piecewise_normal_data,
 )
 
