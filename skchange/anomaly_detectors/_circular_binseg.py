b0ef95956995262a2c54888b24e5a45801b9c0d3
tveten <tveten@nr.no>
2025-08-12 12:30:03 +0200

    docs(circular binseg): use generate_piecewise_normal_data in example

--- skchange/anomaly_detectors/_circular_binseg.py
+++ skchange/anomaly_detectors/_circular_binseg.py
@@ -166,13 +166,17 @@ class CircularBinarySegmentation(BaseSegmentAnomalyDetector):
     Examples
     --------
     >>> from skchange.anomaly_detectors import CircularBinarySegmentation
-    >>> from skchange.datasets import generate_alternating_data
-    >>> df = generate_alternating_data(n_segments=5, mean=10, segment_length=20)
+    >>> from skchange.datasets import generate_piecewise_normal_data
+    >>> df = generate_piecewise_normal_data(
+    ...     means=[0, 10, 0, 20, 0],
+    ...     lengths=[100, 20, 100, 10, 100],
+    ...     random_state=2,
+    ... )
     >>> detector = CircularBinarySegmentation()
     >>> detector.fit_predict(df)
-          ilocs  labels
-    0  [20, 40)       1
-    1  [60, 80)       2
+            ilocs  labels
+    0  [100, 120)       1
+    1  [220, 230)       2
 
     Notes
     -----
