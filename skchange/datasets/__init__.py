a32c26ceb1381e782ed6fc7ed78eec1cfc33e15d
tveten <tveten@nr.no>
2025-08-14 20:46:24 +0200

    add containers for different type of dataset generators

--- skchange/datasets/__init__.py
+++ skchange/datasets/__init__.py
@@ -4,7 +4,10 @@ from ._data_loaders import load_hvac_system_data
 from ._generate import (
     generate_piecewise_data,
 )
-from ._generate_linear_trend import generate_continuous_piecewise_linear_signal
+from ._generate_linear_trend import (
+    generate_continuous_piecewise_linear_data,
+    generate_continuous_piecewise_linear_signal,
+)
 from ._generate_normal import (
     generate_alternating_data,
     generate_anomalous_data,
@@ -13,13 +16,24 @@ from ._generate_normal import (
 )
 from ._generate_regression import generate_piecewise_regression_data
 
+DATA_LOADERS = [
+    load_hvac_system_data,
+]
+GENERATORS = [
+    generate_continuous_piecewise_linear_data,
+    generate_piecewise_data,
+    generate_piecewise_normal_data,
+    generate_piecewise_regression_data,
+]
+OLD_GENERATORS = [
+    generate_alternating_data,
+    generate_anomalous_data,
+    generate_changing_data,
+    generate_continuous_piecewise_linear_signal,
+]
+
 __all__ = [
-    "generate_anomalous_data",
-    "generate_alternating_data",
-    "generate_changing_data",
-    "generate_continuous_piecewise_linear_signal",
-    "load_hvac_system_data",
-    "generate_piecewise_data",
-    "generate_piecewise_normal_data",
-    "generate_piecewise_regression_data",
+    "DATA_LOADERS",
+    "GENERATORS",
+    "OLD_GENERATORS",
 ]
