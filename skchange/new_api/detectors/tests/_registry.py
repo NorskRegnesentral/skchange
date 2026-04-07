caa414c9d0fbc185ea15500c66ebb9f181abcb6a
tveten <tveten@nr.no>
2026-04-07 06:49:10 +0200

    refactor: reorganise detector test registry
    more readable

--- skchange/new_api/detectors/tests/_registry.py
+++ skchange/new_api/detectors/tests/_registry.py
@@ -11,6 +11,9 @@ from skchange.new_api.interval_scorers.tests._registry import (
     INTERVAL_SCORER_TEST_INSTANCES,
 )
 
+# ---------------------------------------------------------------------------
+# Filtered scorer instances for use in detectors
+# ---------------------------------------------------------------------------
 _PENALISED_SAVINGS = [
     scorer
     for scorer in INTERVAL_SCORER_TEST_INSTANCES
@@ -22,20 +25,28 @@ _PENALISED_CHANGE_SCORES = [
     if is_penalised_score(scorer) and is_change_score(scorer)
 ]
 
-DETECTOR_TEST_INSTANCES = (
-    [
-        MovingWindow(),
-        MovingWindow(selection_method="detection_length"),
-        MovingWindow(bandwidth=[2, 3, 5]),
-    ]
-    + [MovingWindow(scorer) for scorer in _PENALISED_CHANGE_SCORES]
-    + [
-        CAPA(),
-        CAPA(min_segment_length=10, max_segment_length=100),
-    ]
-    + [CAPA(segment_saving=scorer) for scorer in _PENALISED_SAVINGS]
-    + [
+# ---------------------------------------------------------------------------
+# Detector test instances
+# They should span a range of configurations, including all compatible scorers.
+# ---------------------------------------------------------------------------
+_MOVING_WINDOW_INSTANCES = [
+    MovingWindow(),
+    MovingWindow(selection_method="detection_length"),
+    MovingWindow(bandwidth=[2, 3, 5]),
+    *[MovingWindow(scorer) for scorer in _PENALISED_CHANGE_SCORES],
+]
+
+_CAPA_INSTANCES = [
+    CAPA(),
+    CAPA(min_segment_length=10, max_segment_length=100),
+    *[CAPA(segment_saving=scorer) for scorer in _PENALISED_SAVINGS],
+    *[
         CAPA(segment_saving=scorer, point_saving=scorer, include_point_anomalies=True)
         for scorer in _PENALISED_SAVINGS
-    ]
-)
+    ],
+]
+
+DETECTOR_TEST_INSTANCES = [
+    *_MOVING_WINDOW_INSTANCES,
+    *_CAPA_INSTANCES,
+]
