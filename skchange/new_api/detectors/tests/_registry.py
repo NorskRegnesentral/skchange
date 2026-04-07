3f0650e06f80c01c3e8cff364f6f8096ba42b9c6
tveten <tveten@nr.no>
2026-04-07 09:37:04 +0200

    feat: add pelt to the new api

--- skchange/new_api/detectors/tests/_registry.py
+++ skchange/new_api/detectors/tests/_registry.py
@@ -2,8 +2,10 @@
 
 from skchange.new_api.detectors._capa import CAPA
 from skchange.new_api.detectors._moving_window import MovingWindow
+from skchange.new_api.detectors._pelt import PELT
 from skchange.new_api.interval_scorers import (
     is_change_score,
+    is_cost,
     is_penalised_score,
     is_saving,
 )
@@ -11,42 +13,46 @@ from skchange.new_api.interval_scorers.tests._registry import (
     INTERVAL_SCORER_TEST_INSTANCES,
 )
 
-# ---------------------------------------------------------------------------
-# Filtered scorer instances for use in detectors
-# ---------------------------------------------------------------------------
-_PENALISED_SAVINGS = [
-    scorer
-    for scorer in INTERVAL_SCORER_TEST_INSTANCES
-    if is_penalised_score(scorer) and is_saving(scorer)
-]
-_PENALISED_CHANGE_SCORES = [
-    scorer
-    for scorer in INTERVAL_SCORER_TEST_INSTANCES
-    if is_penalised_score(scorer) and is_change_score(scorer)
-]
-
-# ---------------------------------------------------------------------------
-# Detector test instances
-# They should span a range of configurations, including all compatible scorers.
-# ---------------------------------------------------------------------------
 _MOVING_WINDOW_INSTANCES = [
     MovingWindow(),
     MovingWindow(selection_method="detection_length"),
     MovingWindow(bandwidth=[2, 3, 5]),
-    *[MovingWindow(scorer) for scorer in _PENALISED_CHANGE_SCORES],
+    *[
+        MovingWindow(scorer)
+        for scorer in INTERVAL_SCORER_TEST_INSTANCES
+        if is_penalised_score(scorer) and is_change_score(scorer)
+    ],
 ]
 
 _CAPA_INSTANCES = [
     CAPA(),
     CAPA(min_segment_length=10, max_segment_length=100),
-    *[CAPA(segment_saving=scorer) for scorer in _PENALISED_SAVINGS],
+    *[
+        CAPA(segment_saving=scorer)
+        for scorer in INTERVAL_SCORER_TEST_INSTANCES
+        if is_penalised_score(scorer) and is_saving(scorer)
+    ],
     *[
         CAPA(segment_saving=scorer, point_saving=scorer, include_point_anomalies=True)
-        for scorer in _PENALISED_SAVINGS
+        for scorer in INTERVAL_SCORER_TEST_INSTANCES
+        if is_penalised_score(scorer) and is_saving(scorer)
+    ],
+]
+
+_PELT_INSTANCES = [
+    PELT(),
+    PELT(min_segment_length=5),
+    PELT(penalty=10.0),
+    PELT(step_size=2),
+    *[
+        PELT(cost=scorer)
+        for scorer in INTERVAL_SCORER_TEST_INSTANCES
+        if not is_penalised_score(scorer) and is_cost(scorer)
     ],
 ]
 
 DETECTOR_TEST_INSTANCES = [
     *_MOVING_WINDOW_INSTANCES,
     *_CAPA_INSTANCES,
+    *_PELT_INSTANCES,
 ]
