b3f972384fa6826c6fe9a424a3e331736672ca4e
tveten <tveten@nr.no>
2026-08-09 10:33:58 +0200

    doc: fix fit_predict header

--- skchange/new_api/detectors/_base.py
+++ skchange/new_api/detectors/_base.py
@@ -63,7 +63,7 @@ class BaseChangeDetector(BaseEstimator):
         )
 
     def fit_predict(self, X, y: ArrayLike | None = None, **fit_params) -> np.ndarray:
-        """Fit to data, then predict per-sample segment labels.
+        """Fit to data, then predict changepoints on the same data.
 
         Equivalent to calling fit(X, y).predict(X).
 
