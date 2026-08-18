ad0f25d74e59a7f09bfde8d6fbf290a5631c352b
tveten <tveten@nr.no>
2026-08-18 15:35:26 +0200

    feat: add the FPOP detector

--- skchange/detectors/__init__.py
+++ skchange/detectors/__init__.py
@@ -4,6 +4,7 @@ from skchange.detectors._base import BaseChangeDetector, is_change_detector
 from skchange.detectors._capa import CAPA
 from skchange.detectors._circular_binseg import CircularBinarySegmentation
 from skchange.detectors._crops import CROPS
+from skchange.detectors._fpop import FPOP
 from skchange.detectors._moving_window import MovingWindow
 from skchange.detectors._pelt import PELT
 from skchange.detectors._seeded_binseg import SeededBinarySegmentation
@@ -13,6 +14,7 @@ __all__ = [
     "CAPA",
     "CircularBinarySegmentation",
     "CROPS",
+    "FPOP",
     "MovingWindow",
     "PELT",
     "SeededBinarySegmentation",
